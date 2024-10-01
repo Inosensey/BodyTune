976ef5cb6b28aa20f254dc1a5ae449f5cb8d82df
Philip Mathew <dingcong.bae@gmail.com>
2024-10-01 12:37:40 +0800

    Sign In Component Added
    This will be the main container of the sign in page.

---
+++ components/authComponents/signIn/SignIn.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SignIn = () => {
+  return (
+    <div
+      data-testid="signIn-container"
+      className="bg-primary max-w-[500px] phone:w-[95%] p-2 rounded-sm"
+    >
+      <header className="text-center font-dmSans font-bold text-lg">
+        Login to BodyTune
+      </header>
+    </div>
+  );
+};
+
+export default SignIn;
