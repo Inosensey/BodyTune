960265d47c45b4a368b967e3f551624b5e3a53f8
Philip Mathew <dingcong.bae@gmail.com>
2024-10-01 11:32:23 +0800

    Update Tailwind configuration
    - Add responsive design support
    - Introduce new color palettes
    - Include new Google fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,10 +7,27 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      phone: '280px',
+      mdphone: '420px',
+      tablet: '481px',
+      laptop: '992px',
+      desktop: '1280px',
+      larger: '1920px'
+    },
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: '#1A2421',
+        lightPrimary: '#2C3A37',
+        secondary: '#415a77',
+        lightSecondary: '#778da9',
+        textGray: '#e0e1dd'
+      },
+      fontFamily: {
+        dmSans: ['var(--font-dm-sans)'],
+        quickSand: ['var(--font-quicksand)'],
       },
     },
   },
