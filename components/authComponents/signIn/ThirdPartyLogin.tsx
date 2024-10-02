8036f3a8c30a9c7cde8bdc7249851cc4b2dc776f
Philip Mathew <dingcong.bae@gmail.com>
2024-10-02 11:52:04 +0800

    UI Updates
    Added props. Updated the html structure. Applied Framer Motion.

--- components/authComponents/signIn/ThirdPartyLogin.tsx
+++ components/authComponents/signIn/ThirdPartyLogin.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+"use client";
+import { motion } from "framer-motion";
 
-const ThirdPartyLogin = () => {
-  return (
-    <div>ThirdPartyLogin</div>
-  )
+interface props {
+  // provider: string;
+  buttonName: string;
+  backgroundColor: string;
+  Icon: React.ComponentType<{ color: string; width?: string; height?: string }>;
+  textBackground: string;
+  testId: string
 }
 
-export default ThirdPartyLogin
+const ThirdPartyLogin = ({
+  Icon,
+  backgroundColor,
+  buttonName,
+  textBackground,
+  testId
+}: props) => {
+  return (
+    <motion.div
+    data-testid={`${testId}`}
+      whileHover={{
+        scale: 1.1,
+        transition: { duration: 0.2 },
+      }}
+      whileTap={{ scale: 0.9 }}
+      className="border-2 border-secondary w-full rounded-md "
+    >
+      <button className="w-full px-1 py-2 font-semibold font-dmSans flex items-center gap-5 pl-5">
+        <Icon color={backgroundColor} height="1.4em" width="1.4em" />{" "}
+        <span>
+          Login with{" "}
+          <span
+            style={{
+              background: `${textBackground}`,
+              color: "transparent",
+              backgroundClip: "text",
+            }}
+          >
+            {buttonName}
+          </span>
+        </span>
+      </button>
+    </motion.div>
+  );
+};
+
+export default ThirdPartyLogin;
