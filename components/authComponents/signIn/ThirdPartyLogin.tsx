6d038d290c8ff872156126eb9b0928e359e021e2
Philip Mathew <dingcong.bae@gmail.com>
2024-10-04 15:02:21 +0800

    Small CSS changes.

--- components/authComponents/signIn/ThirdPartyLogin.tsx
+++ components/authComponents/signIn/ThirdPartyLogin.tsx
@@ -19,15 +19,15 @@ const ThirdPartyLogin = ({
 }: props) => {
   return (
     <motion.div
-    data-testid={`${testId}`}
+      data-testid={`${testId}`}
       whileHover={{
         scale: 1.1,
         transition: { duration: 0.2 },
       }}
       whileTap={{ scale: 0.9 }}
-      className="border-[1px] border-secondary w-full rounded-md "
+      className="border-[1px] border-secondary w-full rounded-md"
     >
-      <button className="w-full px-1 py-2 font-semibold font-dmSans flex items-center gap-5 pl-5">
+      <button className="w-full px-1 py-2 font-semibold font-dmSans flex items-center gap-5 pl-12 text-center">
         <Icon color={backgroundColor} height="1.4em" width="1.4em" />{" "}
         <span>
           Login with{" "}
