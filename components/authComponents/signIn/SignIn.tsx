a29622d2f96173263f4d60952e42810f1c0264d1
Philip Mathew <dingcong.bae@gmail.com>
2024-10-07 14:53:09 +0800

    Updated the HTML structure. Made small CSS adjustments. Added the new Action prop to the ThirdPartyLogin component.

--- components/authComponents/signIn/SignIn.tsx
+++ components/authComponents/signIn/SignIn.tsx
@@ -2,17 +2,16 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
-
 import Image from "next/image";
 
 // Components
-import ThirdPartyLogin from "@/components/authComponents/signIn/ThirdPartyLogin";
+import ThirdPartyLogin from "@/components/authComponents/ThirdPartyLogin";
+import { Input } from "@/components/reusableComponent/formInputs/input";
 
 // Icons
 import PhFacebookLogoBold from "@/icons/PhFacebookLogoBold";
 import PhGoogleLogoBold from "@/icons/PhGoogleLogoBold";
 import PhInstagramLogoBold from "@/icons/PhInstagramLogoBold";
-import { Input } from "@/components/reusableComponent/formInputs/input";
 
 // Types
 interface credentials {
@@ -61,6 +60,7 @@ const SignIn = () => {
             <ThirdPartyLogin
               testId="facebook-third-party-login"
               Icon={PhFacebookLogoBold}
+              action="Login with"
               buttonName="Facebook"
               backgroundColor="#1877F2"
               textBackground="linear-gradient(90deg, #1877F2, #1877F2)"
@@ -70,6 +70,7 @@ const SignIn = () => {
             <ThirdPartyLogin
               testId="google-third-party-login"
               Icon={PhGoogleLogoBold}
+              action="Login with"
               buttonName="Google"
               backgroundColor="#DB4437"
               textBackground="linear-gradient(90deg, #4285F4, #DB4437, #F4B400, #0F9D58)"
@@ -79,97 +80,98 @@ const SignIn = () => {
             <ThirdPartyLogin
               testId="instagram-third-party-login"
               Icon={PhInstagramLogoBold}
+              action="Login with"
               buttonName="Instagram"
               backgroundColor="#FD1D1D"
               textBackground="linear-gradient(90deg, #833AB4, #E1306C, #FD1D1D, #FCAF45)"
             />
           </div>
         </div>
-        <hr
-          style={{ borderColor: "#b3b3b3" }}
-          className="max-w-[450px] phone:w-10/12 my-4"
-        />
-        <div className="flex flex-col justify-center gap-4 max-w-[450px] phone:w-8/12">
-          <form
-            data-testid="credentials-login-form"
-            className="flex flex-col gap-2"
-          >
-            <div className="w-full">
-              <Input
-                name="email"
-                dataTestId="email-input"
-                placeholder="Enter your Email"
-                state={credentials.email}
-                type="email"
-                label="Email"
-                onChange={inputOnChange}
-                autoComplete="off"
-              />
-            </div>
-            <div className="w-full relative">
-              <Input
-                dataTestId="password-input"
-                name="password"
-                placeholder="Enter your Password"
-                state={credentials.password}
-                type="password"
-                label="Password"
-                onChange={inputOnChange}
-                autoComplete="off"
-              />
-            </div>
-          </form>
-          <div className="flex justify-between items-center">
-            {/* switch */}
-            <div className="flex items-center gap-2">
-              <div
-                data-testid="remember-me"
-                style={{
-                  justifyContent: rememberMe ? "flex-end" : "flex-start",
-                  background: rememberMe ? "#4B6F64" : "#FFFFFF66",
-                }}
-                className="w-[40px] h-[20px] bg-[#FFFFFF66] flex justify-start rounded-[50px] py-[3px] px-[5px] cursor-pointer"
-                onClick={toggleRememberMe}
-              >
-                <motion.div
-                  className="w-[15px] h-[15px] bg-white rounded-[40px]"
-                  layout
-                  transition={{ type: "spring", stiffness: 700, damping: 50 }}
-                />
-              </div>
-              <label className="phone:text-sm font-semibold font-dmSans">
-                Remember Me
-              </label>
-            </div>
-            <p
-              data-testid="forgot-password"
-              className="text-sm font-dmSans underline cursor-pointer font-semibold"
-            >
-              Forgot your password?
-            </p>
+      </div>
+      <hr
+        style={{ borderColor: "#b3b3b3" }}
+        className="max-w-[450px] phone:w-10/12 my-4 mx-auto"
+      />
+      <div className="flex flex-col justify-center mx-auto gap-4 max-w-[450px] phone:w-8/12">
+        <form
+          data-testid="credentials-login-form"
+          className="flex flex-col gap-2"
+        >
+          <div className="w-full">
+            <Input
+              name="email"
+              dataTestId="email-input"
+              placeholder="Enter your Email"
+              state={credentials.email}
+              type="email"
+              label="Email"
+              onChange={inputOnChange}
+              autoComplete="off"
+            />
           </div>
-          <div className="w-28 mx-auto" data-testid="credentials-login-button">
-            <motion.button
-              whileHover={{
-                scale: 1.1,
-                transition: { duration: 0.2 },
+          <div className="w-full relative">
+            <Input
+              dataTestId="password-input"
+              name="password"
+              placeholder="Enter your Password"
+              state={credentials.password}
+              type="password"
+              label="Password"
+              onChange={inputOnChange}
+              autoComplete="off"
+            />
+          </div>
+        </form>
+        <div className="flex justify-between items-center">
+          {/* switch */}
+          <div className="flex items-center gap-2">
+            <div
+              data-testid="remember-me"
+              style={{
+                justifyContent: rememberMe ? "flex-end" : "flex-start",
+                background: rememberMe ? "#4B6F64" : "#FFFFFF66",
               }}
-              whileTap={{ scale: 0.9 }}
-              className="bg-secondary text-white font-quickSand font-bold w-full rounded-md p-1"
+              className="w-[40px] h-[20px] bg-[#FFFFFF66] flex justify-start rounded-[50px] py-[3px] px-[5px] cursor-pointer"
+              onClick={toggleRememberMe}
             >
-              Login
-            </motion.button>
+              <motion.div
+                className="w-[15px] h-[15px] bg-white rounded-[40px]"
+                layout
+                transition={{ type: "spring", stiffness: 700, damping: 50 }}
+              />
+            </div>
+            <label className="phone:text-sm font-semibold font-dmSans">
+              Remember Me
+            </label>
           </div>
-          <div className="text-center" data-testid="create-account-link">
-            <p className="text-sm font-dmSans">
-              Don&apos;t have an account yet?{" "}
-              <Link href={"/register"}>
-                <span className=" underline cursor-pointer font-bold text-secondary">
-                  Create an Account
-                </span>
-              </Link>
-            </p>
-          </div>
+          <p
+            data-testid="forgot-password"
+            className="text-sm font-dmSans underline cursor-pointer font-semibold"
+          >
+            Forgot your password?
+          </p>
+        </div>
+        <div className="w-28 mx-auto" data-testid="credentials-login-button">
+          <motion.button
+            whileHover={{
+              scale: 1.1,
+              transition: { duration: 0.2 },
+            }}
+            whileTap={{ scale: 0.9 }}
+            className="bg-secondary text-white font-quickSand font-bold w-full rounded-md p-1"
+          >
+            Login
+          </motion.button>
+        </div>
+        <div className="text-center" data-testid="create-account-link">
+          <p className="text-sm font-dmSans">
+            Don&apos;t have an account yet?{" "}
+            <Link href={"/register"}>
+              <span className=" underline cursor-pointer font-bold text-secondary">
+                Create an Account
+              </span>
+            </Link>
+          </p>
         </div>
       </div>
     </div>
