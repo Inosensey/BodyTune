b3b9d3d469fda4a5fa25617e91325b7a89c310b3
Philip Mathew <dingcong.bae@gmail.com>
2024-10-10 14:34:01 +0800

    Sign-up UI updates.
    - Corrected minor typos in the interface props.
    - Created an array for months.
    - Added two onChange events for the select and radio inputs.
    - Updated the Select element to use options from the months array, with minor CSS adjustments.
    - Completed two custom radio buttons.

--- components/authComponents/signUp/steps/ThirdStep.tsx
+++ components/authComponents/signUp/steps/ThirdStep.tsx
@@ -13,7 +13,7 @@ interface props {
   birthDay: string;
   birthMonth: string;
   birthYear: string;
-  Gender: string;
+  gender: string;
   setRegisterInput: React.Dispatch<React.SetStateAction<registerInputType>>;
   progressTitle: string;
   currentProgress: number;
@@ -21,24 +21,41 @@ interface props {
   setProgress: React.Dispatch<React.SetStateAction<number>>;
 }
 
+const months:Array<string> = [
+  "January", "February", "March", "April", "May", "June", 
+  "July", "August", "September", "October", "November", "December"
+];
+
+
 const ThirdStep = ({
   name,
   birthDay,
   birthMonth,
   birthYear,
-  Gender,
+  gender,
   setRegisterInput,
   progressTitle,
   currentProgress,
   totalProgress,
   setProgress,
 }: props) => {
+  // States
+
   // Events
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
 
     setRegisterInput((prev) => ({ ...prev, [name]: value }));
   };
+  const radioOnChange = (value: string, name: string) => {
+    setRegisterInput((prev) => ({ ...prev, [name]: value }));
+  };
+  const selectOnChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const { name, value } = event.target;
+
+    setRegisterInput((prev) => ({ ...prev, [name]: value }));
+  }
+
   return (
     <div className="flex flex-col justify-center mx-auto gap-8 max-w-[450px] phone:w-8/12 mt-4">
       <div
@@ -74,53 +91,113 @@ const ThirdStep = ({
           </div>
         </div>
         <div className="w-full">
-            <div className="flex flex-col">
-                <label className="phone:text-sm font-quickSand font-semibold">Date of birth</label>
-                <p className="text-[#999999] text-xs font-semibold font-dmSans">Why do we need your date of birth? Learn more.</p>
+          <div className="flex flex-col">
+            <label className="phone:text-sm font-quickSand font-semibold">
+              Date of birth
+            </label>
+            <p className="text-[#999999] text-xs font-semibold font-dmSans">
+              Why do we need your date of birth? Learn more.
+            </p>
+          </div>
+          <div className="flex items-center gap-1 w-full">
+            <div className="w-4/12 relative">
+              <Input
+                name={birthDay}
+                placeholder="dd"
+                state={name}
+                type="text"
+                onChange={onChange}
+                autoComplete="off"
+              />
             </div>
-            <div className="flex items-center gap-1 w-full">
-                <div className="w-4/12 relative">
-                    <Input
-                        name={birthDay}
-                        placeholder="dd"
-                        state={name}
-                        type="text"
-                        onChange={onChange}
-                        autoComplete="off"
-                    />
-                </div>
-                <div className="w-full relative mt-[0.45rem]">
-                    <div className={`flex flex-col phone:w-[96%] mdphone:w-11/12 laptop:w-full gap-2 bg-primary`}>
-                        <select className={`bg-transparent w-[92%]text-white h-[2.7rem] phone:text-sm font-quickSand`} value={birthMonth}>
-                            <option value="" disabled selected>Month</option>
-                            <option>test</option>
-                            <option>test</option>
-                        </select>
-                    </div>
-                </div>
-                <div className="w-5/12/12 relative">
-                    <Input
-                        name={birthYear}
-                        placeholder="yyyy"
-                        state={name}
-                        type="text"
-                        onChange={onChange}
-                        autoComplete="off"
-                    />
-                </div>
+            <div className="w-full relative mt-[0.45rem]">
+              <div
+                className={`flex flex-col phone:w-[96%] mdphone:w-11/12 laptop:w-full gap-2 bg-primary`}
+              >
+                <select
+                  className={`bg-transparent w-[92%]text-white h-[2.7rem] phone:text-sm font-quickSand`}
+                  onChange={selectOnChange}
+                  name="birthMonth"
+                  defaultValue={birthMonth}
+                >
+                  <option className="bg-primary font-quickSand" value="" disabled>
+                    Month
+                  </option>
+                  {months.map((month:string) => (
+                    
+                    <option className="bg-primary font-quickSand" key={month} value={month}>{month}</option>
+                  ))}
+                </select>
+              </div>
             </div>
+            <div className="w-5/12/12 relative">
+              <Input
+                name={birthYear}
+                placeholder="yyyy"
+                state={name}
+                type="text"
+                onChange={onChange}
+                autoComplete="off"
+              />
+            </div>
+          </div>
         </div>
         <div className="w-full">
-            <div className="flex flex-col">
-                <label className="phone:text-sm font-quickSand font-semibold">Gender</label>
-                <p className="text-[#999999] text-xs font-semibold font-dmSans">Why do we need your date of birth?We use your gender to help personalize our content recommendations for you.</p>
+          <div className="flex flex-col">
+            <label className="phone:text-sm font-quickSand font-semibold">
+              Gender
+            </label>
+            <p className="text-[#999999] text-xs font-semibold font-dmSans">
+              Why do we need your date of birth?We use your gender to help
+              personalize our content recommendations for you.
+            </p>
+          </div>
+          <div className="w-full flex items-center flex-wrap gap-2 mt-2">
+            <div
+              className="flex items-center gap-1 cursor group font-dmSans"
+              onClick={() => radioOnChange("Male", "gender")}
+            >
+              <div
+                style={{
+                  boxShadow:
+                    gender === "Male" ? "0px 0px 0px 3.5px #4B6F64 inset" : "",
+                }}
+                className="w-4 h-4 rounded-2xl shadow-[0px_0px_0px_2px_#fff_inset] group-hover:shadow-[0px_0px_0px_2px_#4B6F64_inset]"
+              ></div>
+              <label>Male</label>
+              <input
+                type="radio"
+                name="gender"
+                value="Male"
+                className="hidden"
+                onChange={() => console.log("wew")}
+                checked={gender === "Male" ? true : false}
+              />
             </div>
-            <div className="w-full">
-                <div className="flex items-center gap-2 cursor group">
-                    <div className="border-[1px] w-4 h-4 rounded-2xl group-hover:border-secondary"></div>
-                    <input type="radio" name="" id="" />
-                </div>
+            <div
+              className="flex items-center gap-1 cursor group font-dmSans"
+              onClick={() => radioOnChange("Female", "gender")}
+            >
+              <div
+                style={{
+                  boxShadow:
+                    gender === "Female"
+                      ? "0px 0px 0px 3.5px #4B6F64 inset"
+                      : "",
+                }}
+                className="w-4 h-4 rounded-2xl shadow-[0px_0px_0px_2px_#fff_inset] group-hover:shadow-[0px_0px_0px_2px_#4B6F64_inset]"
+              ></div>
+              <label>Female</label>
+              <input
+                type="radio"
+                name="gender"
+                value="Female"
+                className="hidden"
+                onChange={() => console.log("wew")}
+                checked={gender === "Female" ? true : false}
+              />
             </div>
+          </div>
         </div>
       </div>
     </div>
