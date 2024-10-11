e7426cb78c3a06045505ebd080bcd6b4d1794061
Philip Mathew <dingcong.bae@gmail.com>
2024-10-11 11:17:09 +0800

    Added data-testid attribute for Jest tests in the ProgressBar.

--- components/authComponents/signUp/ProgressBar.tsx
+++ components/authComponents/signUp/ProgressBar.tsx
@@ -1,25 +1,34 @@
-"use client"
+"use client";
 
 interface props {
-    currentProgress: number,
-    totalProgress: number
+  currentProgress: number;
+  totalProgress: number;
 }
 
-const ProgressBar = ({currentProgress, totalProgress}:props) => {
+const ProgressBar = ({ currentProgress, totalProgress }: props) => {
+  const calculateProgress = (
+    currentProgress: number = 0,
+    totalProgress: number = 0
+  ): number => {
+    let progress: number = 0;
 
-    const calculateProgress = (currentProgress: number = 0, totalProgress: number = 0):number => {
-        let progress: number = 0
+    progress = (currentProgress / totalProgress) * 100;
 
-        progress = (currentProgress / totalProgress) * 100;
-
-        return progress;
-    }
+    return progress;
+  };
 
   return (
-    <div className="h-[3px] w-full bg-[#b3b3b3] rounded-sm relative">
-        <div style={{width: `${calculateProgress(currentProgress, totalProgress)}%`}} className={`h-[3px] absolute top-0 left-0 bg-secondary`}></div>
+    <div
+      className="h-[3px] w-full bg-[#b3b3b3] rounded-sm relative"
+    >
+      <div
+        style={{
+          width: `${calculateProgress(currentProgress, totalProgress)}%`,
+        }}
+        className={`h-[3px] absolute top-0 left-0 bg-secondary`}
+      ></div>
     </div>
-  )
-}
+  );
+};
 
-export default ProgressBar
+export default ProgressBar;
