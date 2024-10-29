3fdc2c95f80719685f100b2cf4c152893c2efce5
Inosensey <dingcong.bae@gmail.com>
2024-10-29 19:20:32 +0800

    Add validation types to inputTypes.

--- types/inputTypes.ts
+++ types/inputTypes.ts
@@ -9,3 +9,23 @@ export interface registerInputType {
   weight: string,
   height: string,
 }
+
+export interface stepsValidation {
+  isFirstStepValid: boolean,
+  isSecondStepValid: boolean,
+  isThirdStepValid: boolean,
+  isFourthStepValid: boolean
+}
+export interface validation {
+  validationName?: string;
+  valid: null | boolean;
+  validationMessage: string;
+}
+
+export interface stepValidationResult {
+  [key: string]: {
+    validationName: string;
+    valid: boolean | null;
+    validationMessage: string;
+  };
+}
