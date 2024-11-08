8b834341022575b868f0ce3c4f3c165e971a8d3d
Philip Mathew <dingcong.bae@gmail.com>
2024-11-08 13:58:00 +0800

    Updated ThirdPartyLogin component: imported form actions and hooks, added provider prop, and restructured HTML with formAction attribute in form tag for improved OAuth flow.

--- components/authComponents/ThirdPartyLogin.tsx
+++ components/authComponents/ThirdPartyLogin.tsx
@@ -1,5 +1,14 @@
 "use client";
+import { useEffect } from "react";
+import { useFormState } from "react-dom";
 import { motion } from "framer-motion";
+import { useRouter } from "next/navigation";
+
+// Actions
+import { loginWithThirdParty } from "@/actions/authActions";
+
+// Types
+import { formReturnType } from "@/types/formTypes";
 
 interface props {
   // provider: string;
@@ -8,8 +17,19 @@ interface props {
   Icon: React.ComponentType<{ color: string; width?: string; height?: string }>;
   textBackground: string;
   action: string,
-  testId: string
+  testId: string,
+  provider: string
 }
+
+// Initials
+const useFormStateInitials: formReturnType<
+  { redirectLink: string; loginType: string } | []
+> = {
+  success: null,
+  error: null,
+  message: "",
+  data: [],
+};
 
 const ThirdPartyLogin = ({
   Icon,
@@ -17,10 +37,28 @@ const ThirdPartyLogin = ({
   buttonName,
   textBackground,
   action,
-  testId
+  testId,
+  provider
 }: props) => {
+  // Initialize router
+  const router = useRouter()
+
+  // UseFormState
+  const [state, formAction] = useFormState(
+    loginWithThirdParty,
+    useFormStateInitials
+  );
+
+  useEffect(() => {
+    if(state.success !== null || state.error !== null) {
+      if (!Array.isArray(state.data)) {
+        router.push(state.data.redirectLink)
+      }
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[state])
   return (
-    <motion.div
+    <motion.form
       data-testid={`${testId}`}
       whileHover={{
         scale: 1.1,
@@ -28,7 +66,9 @@ const ThirdPartyLogin = ({
       }}
       whileTap={{ scale: 0.9 }}
       className="border-[1px] border-secondary w-full rounded-md flex justify-center"
+      action={formAction}
     >
+      <input type="hidden" name="provider" value={provider} />
       <button className="px-1 py-2 font-semibold font-dmSans flex items-center gap-2  text-center">
         <Icon color={backgroundColor} height="1.4em" width="1.4em" />{" "}
         <span>
@@ -44,7 +84,7 @@ const ThirdPartyLogin = ({
           </span>
         </span>
       </button>
-    </motion.div>
+    </motion.form>
   );
 };
 
