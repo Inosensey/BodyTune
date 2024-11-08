21db5986f5cbfc6bf06c02560061cc30c75890be
Philip Mathew <dingcong.bae@gmail.com>
2024-11-08 13:57:34 +0800

    Added a dashboard page as the main user interface.

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+"use server";
+
+import SignOut from "@/components/authComponents/SignOut";
+
+
+const DashboardPage = () => {
+  return (
+    <div>
+      DashboardPage{" "}
+        <SignOut />
+    </div>
+  );
+};
+
+export default DashboardPage;
