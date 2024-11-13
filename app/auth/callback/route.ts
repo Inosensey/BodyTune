54cfb1add4b5dba7ef0397e44f88acfd334aaab8
Philip Mathew <dingcong.bae@gmail.com>
2024-11-13 13:50:15 +0800

    Updated GET request handling: added check for existing user information and created cookie for check result.

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,6 +1,34 @@
 import { NextResponse } from 'next/server'
+import { cookies } from 'next/headers'
 // The client you created from the Server-Side Auth instructions
 import { createSSR } from '@/utils/supabaseSSR'
+
+const checkIfUserHaverPersonalInformation = async (userId: string) => {
+  const supabase = await createSSR()
+  try {
+    const {data, error} = await supabase.from("personal_information").select("id").eq("user_id", userId)
+    
+    if (error) {
+      const errorMessage: string = `There is an error getting your personal Information: ${error.message}`;
+      return {
+        message: errorMessage,
+      };
+    }
+    if(data.length === 1) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error: unknown) {
+    const errorMessage: string =
+      error instanceof Error
+        ? `There is an error getting your personal information: ${error.message}`
+        : "An unknown error occurred";
+    return {
+      message: errorMessage,
+    };
+  }
+}
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
@@ -12,6 +40,16 @@ export async function GET(request: Request) {
     const supabase = await createSSR()
     const { error } = await supabase.auth.exchangeCodeForSession(code)
     if (!error) {
+      const user = await supabase.auth.getUser();
+      const userHaverPersonalInformation = await checkIfUserHaverPersonalInformation(user.data.user!.id)
+      
+      const cookieStore = await cookies()
+      if(userHaverPersonalInformation) {
+        cookieStore.set('userHaverPersonalInformation', "true", { secure: true })
+      } else {
+        cookieStore.set('userHaverPersonalInformation', "false", { secure: true })
+      }
+
       const forwardedHost = request.headers.get('x-forwarded-host') // original origin before load balancer
       const isLocalEnv = process.env.NODE_ENV === 'development'
       if (isLocalEnv) {
