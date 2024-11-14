5df5b516fe810db468ce9bd878af408438a4f5c1
Philip Mathew <dingcong.bae@gmail.com>
2024-11-14 14:51:44 +0800

    Created Sidebar tests to verify presence of required elements.

---
+++ test/Sidebar.test.tsx
@@ -0,0 +1,27 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+
+import Sidebar from "@/components/dashboardComponents/Sidebar";
+import { describe } from "node:test";
+
+describe("Sidebar Component", () => {
+    it("Renders the sidebar component and its contents", () => {
+        render(<Sidebar />)
+
+        
+        // check if all components are rendered
+        expect(screen.getByTestId("header-container")).toBeInTheDocument();
+        expect(screen.getByTestId("links-container")).toBeInTheDocument();
+        expect(screen.getByTestId("overview-link")).toBeInTheDocument();
+        expect(screen.getByTestId("overview-link-description")).toBeInTheDocument();
+        expect(screen.getByTestId("bodytune-studio-link")).toBeInTheDocument();
+        expect(screen.getByTestId("bodytune-studio-link-description")).toBeInTheDocument();
+        expect(screen.getByTestId("bodytune-workouts-link")).toBeInTheDocument();
+        expect(screen.getByTestId("bodytune-workouts-link-description")).toBeInTheDocument();
+        expect(screen.getByTestId("bodytune-nutrition-link")).toBeInTheDocument();
+        expect(screen.getByTestId("bodytune-nutrition-link-description")).toBeInTheDocument();
+        expect(screen.getByTestId("settings-link")).toBeInTheDocument();
+        expect(screen.getByTestId("settings-link-description")).toBeInTheDocument();
+        expect(screen.getByTestId("signOut")).toBeInTheDocument();
+    })
+})
