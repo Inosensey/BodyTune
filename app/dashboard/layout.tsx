cb36c6900243715d0b31d958d3a96bcaebc34f4d
Philip Mathew <dingcong.bae@gmail.com>
2024-11-15 14:47:07 +0800

    Adjusted HTML structure for improved layout.

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -12,9 +12,11 @@ export default function DashboardLayout({
     children: React.ReactNode;
   }>) {
   return (
-    <div className="w-full h-screen relative">
-      {children}
+    <div className="w-full h-screen relative tablet:flex">
       <Sidebar />
+      <div className="flex-1 h-full">
+        {children}
+      </div>
     </div>
   );
 }
