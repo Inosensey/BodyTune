b5e51bd1e28e79120307a33b7331f0fb7d61d06e
Philip Mathew <dingcong.bae@gmail.com>
2024-11-15 14:46:05 +0800

    Created Overview component to contain all overview-related information.

---
+++ components/dashboardComponents/overviewComponents/Overview.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+// Components
+import WeeklyActivity from './WeeklyActivity'
+
+const Overview = () => {
+  return (
+    <div className='w-full h-full'>
+        <WeeklyActivity />
+    </div>
+  )
+}
+
+export default Overview
