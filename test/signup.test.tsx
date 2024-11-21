ee9d65d955acd09510b388d86cfab8fcb13c9489
Philip Mathew <dingcong.bae@gmail.com>
2024-11-21 15:40:21 +0800

    Mocked useRouter and useFormState for the signup tests.

--- test/signup.test.tsx
+++ test/signup.test.tsx
@@ -4,6 +4,35 @@ import { render, screen } from "@testing-library/react";
 import SignUp from "@/components/authComponents/signUp/SignUp";
 import { describe } from "node:test";
 
+jest.mock("next/navigation", () => ({
+  useRouter() {
+    return {
+      prefetch: () => null
+    };
+  }
+}));
+
+jest.mock("react-dom", () => {
+  const originalModule = jest.requireActual("react-dom");
+
+  return {
+    ...originalModule,
+    useFormState: () => [
+      [
+        {
+          // Return a mock state object
+          success: null,
+          error: null,
+          message: "",
+          data: [],
+        },
+        // Mock setState function
+        jest.fn(),
+      ],
+    ],
+  };
+});
+
 describe("Sign Up Component", () => {
   it("Renders the sign up component and its content", () => {
     render(<SignUp />);
