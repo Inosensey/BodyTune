6bf200b061899f7da4545116967d16a976cb5bc5
Philip Mathew <dingcong.bae@gmail.com>
2024-11-25 14:46:05 +0800

    Created a UI for displaying an empty BodyTune state.

--- components/dashboardComponents/bodytuneStudio/BodyTuneStudioContents.tsx
+++ components/dashboardComponents/bodytuneStudio/BodyTuneStudioContents.tsx
@@ -1,8 +1,16 @@
 "use client";
 
+import Image from "next/image";
+
 const BodyTuneStudioContents = () => {
   return (
-    <div className="bg-black w-full p-4 rounded-lg">BodyTuneStudioContents</div>
+    <div className="flex-1 bg-black w-full p-4 rounded-lg">
+      <div className="flex flex-col w-full h-full font-dmSans justify-center items-center">
+        <Image src="/assets/svg/dumbbell-2.svg" width={300} height={300} alt="Logo" />
+        <p className="w-max text-xl">You don&apos;t have a <span className="font-bold font-quickSand text-secondary">BodyTune</span> yet.</p>
+        <p className="w-max text-lg text-lightSecondary underline cursor-pointer">Create your first one now!</p>
+      </div>
+    </div>
   );
 };
 
