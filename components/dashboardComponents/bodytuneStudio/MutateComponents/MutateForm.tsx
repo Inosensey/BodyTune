5cdc668fc351c9ce52e5ddf3c67702e115ebe53c
Inosensey <dingcong.bae@gmail.com>
2024-11-27 16:21:14 +0800

    Integrated BreadCrumbs and FirstStep components into MutateForm.
    Added state management and passed down props to dynamically render content based on user selection.

--- components/dashboardComponents/bodytuneStudio/MutateComponents/MutateForm.tsx
+++ components/dashboardComponents/bodytuneStudio/MutateComponents/MutateForm.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 // Components
 import DashboardHeader from "../../DashboardHeader";
 import BodyTuneCreationOptions from "./BodyTuneCreationOptions";
@@ -9,21 +10,69 @@ import SolarStarsMinimalisticLineDuotone from "@/icons/SolarStarsMinimalisticLin
 
 // Types
 import { TableRow } from "@/types/database.types";
+import FirstStep from "./FirstStep";
+import { InterfaceBreadCrumbs } from "@/types/inputTypes";
+import BreadCrumbs from "./BreadCrumbs";
 interface props {
-  personaInfo: TableRow<"personal_information">
+  personaInfo: TableRow<"personal_information">;
 }
 
-const MutateForm = ({personaInfo}:props) => {
-  console.log(personaInfo)
+// Initials
+const BreadCrumbsInitials: InterfaceBreadCrumbs[] = [
+  {
+    id: 1,
+    title: "Body Metrics",
+    shortDescription: "Set weight, height, and experience",
+  },
+  {
+    id: 2,
+    title: "Meal Plan",
+    shortDescription: "Customize your daily meals",
+  },
+  {
+    id: 3,
+    title: "Exercise Plan",
+    shortDescription: "Define your workout routine",
+  },
+  {
+    id: 4,
+    title: "Finalize & Share",
+    shortDescription: "Review and set visibility",
+  },
+];
+
+const MutateForm = ({ personaInfo }: props) => {
+  const [selectedOption, setSelectedOption] = useState<string>("");
+  const [selectedBreadCrumb, setSelectedBreadCrumb] =
+    useState<InterfaceBreadCrumbs>({
+      id: 1,
+      title: "Body Metrics",
+      shortDescription: "Set weight, height, and experience",
+    });
+
   return (
-    <div className="flex flex-col gap-1 h-[99%]">
+    <div className="flex flex-col gap-4 h-[99%]">
       <DashboardHeader
         headerText="Create Your BodyTune"
         headerDescription="Craft a personalized plan by combining workouts and meals into your perfect routine."
         Icon={SolarStarsMinimalisticLineDuotone}
       />
-      <div className="flex flex-col flex-1 gap-8 items-center justify-center">
-        <BodyTuneCreationOptions />
+      <div className="flex flex-col flex-1 gap-8 items-center">
+        {selectedOption === "" ? (
+          <BodyTuneCreationOptions setSelectOption={setSelectedOption} />
+        ) : (
+          <div className="flex flex-col gap-4">
+            <BreadCrumbs
+              breadCrumbs={BreadCrumbsInitials}
+              setSelectedBreadCrumb={setSelectedBreadCrumb}
+              selectedBreadCrumb={selectedBreadCrumb}
+            />
+            <FirstStep
+              personalInfo={personaInfo}
+              setSelectedOption={setSelectedOption}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
