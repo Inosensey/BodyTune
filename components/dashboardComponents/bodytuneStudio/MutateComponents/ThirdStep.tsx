e9ba106ea9fb813fc3ffab402bc8942960bd60b8
Philip Mathew <dingcong.bae@gmail.com>
2025-01-02 16:55:35 +0800

    Enhanced the HTML structure and added new events and states for better functionality.

--- components/dashboardComponents/bodytuneStudio/MutateComponents/ThirdStep.tsx
+++ components/dashboardComponents/bodytuneStudio/MutateComponents/ThirdStep.tsx
@@ -1,5 +1,9 @@
 "use client";
 import { useState } from "react";
+import {motion} from "framer-motion"
+
+// components
+import { Input } from "@/components/reusableComponent/formInputs/input";
 
 // Icons
 import IcOutlineArrowBackIosNew from "@/icons/IcOutlineArrowBackIosNew";
@@ -12,6 +16,7 @@ interface thirdStepInterface {
   exercisePlanName: string;
 }
 import { InterfaceBreadCrumbs } from "@/types/inputTypes";
+import Image from "next/image";
 interface props {
   setSelectedOption: React.Dispatch<React.SetStateAction<string>>;
   setProgress: React.Dispatch<React.SetStateAction<number>>;
@@ -21,6 +26,7 @@ interface props {
 }
 
 // Initials
+import { weekDates } from "@/utils/initials";
 const thirdStepInitials: thirdStepInterface = {
   selectedExercisePlan: 0,
   exercisePlanName: "",
@@ -31,12 +37,35 @@ const ThirdStep = ({
   setSelectedBreadCrumb,
 }: props) => {
   // States
+  const [selectedWeekDate, setSelectedWeekDate] = useState<string>("Monday");
   const [showExercisePlanHtml, setShowExercisePanHtml] =
     useState<boolean>(false);
   const [thirdStepFieldsVal, setThirdStepFieldsVal] =
     useState<thirdStepInterface>(thirdStepInitials);
 
   // Events
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    // const validationParams = {
+    //   stateName: name,
+    //   value: value,
+    // };
+
+    // const validationResult: validation = FormValidation(validationParams);
+
+    // setMealStepValidations((prev) => ({
+    //   ...prev,
+    //   [validationResult.validationName!]: {
+    //     valid: validationResult.valid,
+    //     validationMessage: validationResult.validationMessage,
+    //   },
+    // }));
+
+    // const allInputValidationResult = checkAllInputValidations();
+    // checkValidations(validationResult);
+    setThirdStepFieldsVal((prev) => ({ ...prev, [name]: value }));
+  };
   const selectOnChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const { name, value, options } = event.target;
 
@@ -71,7 +100,6 @@ const ThirdStep = ({
     <>
       <div className="flex flex-col h-full w-min-[350px]">
         <div
-          style={{ height: `${showExercisePlanHtml && "27%"}` }}
           className="bg-black rounded-t-lg pt-4 py-2 w-full"
         >
           <div
@@ -152,6 +180,92 @@ const ThirdStep = ({
             </div>
           </div>
         </div>
+        {showExercisePlanHtml && (
+          <div className="flex flex-col gap-1 bg-black flex-1 rounded-b-lg pt-2 pb-4 px-2 h-[70%] w-[1150px]">
+            <div className="flex flex-col gap-2 phone:h-[35%] tablet:h-[25%] ">
+              <motion.div className="phone:w-3/12">
+                <Input
+                  name="exercisePlanName"
+                  placeholder="Enter the name of the Exercise Plan"
+                  state={thirdStepFieldsVal.exercisePlanName}
+                  type="text"
+                  label="Exercise Plan Name"
+                  onChange={onChange}
+                  onBlur={onChange}
+                  autoComplete="off"
+                  valid={null}
+                  validationMessage={""}
+                />
+              </motion.div>
+              <div className="flex gap-1">
+                {weekDates.map((date: string, index: number) => (
+                  <div
+                    className="group border-[1.5px] border-secondary px-4 py-1 cursor-pointer"
+                    key={index}
+                    onClick={() => setSelectedWeekDate(date)}
+                  >
+                    <p
+                      className={`text-sm font-semibold font-quickSand transition duration-200 ${
+                        selectedWeekDate === date
+                          ? "text-[#ffffff]"
+                          : "text-[#b3b3b3] group-hover:text-[#ffffff]"
+                      }`}
+                    >
+                      {date}
+                    </p>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="flex-1 flex gap-1 justify-between phone:h-[69%] tablet:h-[89%]">
+              <div className="w-[33%] h-[100%] overflow-auto flex flex-1 border-[1.5px] border-lightSecondary">
+                <div className="w-[100%] flex flex-col justify-center items-center">
+                  <Image
+                    src="/assets/svg/dumbbell-2.svg"
+                    width={200}
+                    height={200}
+                    alt="Logo"
+                  />
+                  <p
+                    className="font-dmSans font-semibold text-lightSecondary underline cursor-pointer"
+                  >
+                    Make a Breakfast Meal
+                  </p>
+                </div>
+              </div>
+              <div className="w-[33%] h-[100%] overflow-auto flex flex-1 border-[1.5px] border-lightSecondary">
+                <div className="w-[100%] flex flex-col justify-center items-center">
+                  <Image
+                    src="/assets/svg/dumbbell-2.svg"
+                    width={200}
+                    height={200}
+                    alt="Logo"
+                  />
+                  <p
+                    className="font-dmSans font-semibold text-lightSecondary underline cursor-pointer"
+                  >
+                    Make a Lunch Meal
+                  </p>
+                </div>
+              </div>
+              <div className="w-[33%] h-[100%] overflow-auto flex flex-1 border-[1.5px] border-lightSecondary">
+                <div className="w-[100%] flex flex-col justify-center items-center">
+                  <Image
+                    src="/assets/svg/dumbbell-2.svg"
+                    width={200}
+                    height={200}
+                    alt="Logo"
+                  />
+                  <p
+                    className="font-dmSans font-semibold text-lightSecondary underline cursor-pointer"
+                  >
+                    Make a Dinner Meal
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
