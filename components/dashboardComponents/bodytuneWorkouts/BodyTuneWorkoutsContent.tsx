482a0e1a36003d544a57c14dfea47b88c9be1c5b
Inosensey <dingcong.bae@gmail.com>
2025-01-16 06:36:16 +0800

    Added BodyTuneWorkoutsContent component for displaying page contents.

---
+++ components/dashboardComponents/bodytuneWorkouts/BodyTuneWorkoutsContent.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+const BodyTuneWorkoutsContent = () => {
+  return (
+    <div className="flex-1 bg-black w-full p-4 rounded-lg">
+      <div className="flex flex-col w-full h-full font-dmSans justify-center items-center">
+        <Image
+          src="/assets/svg/dumbbell-2.svg"
+          width={300}
+          height={300}
+          alt="Logo"
+        />
+        <p className="w-max text-xl">
+          You don&apos;t have any{" "}
+          <span className="font-bold font-quickSand text-secondary">
+            Workouts
+          </span>{" "}
+          yet.
+        </p>
+        <Link href={"bodytune/create"}>
+          <p className="w-max text-lg text-lightSecondary underline cursor-pointer">
+            Create your first one now!
+          </p>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default BodyTuneWorkoutsContent;
