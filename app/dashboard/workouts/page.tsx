1f7fbd490cdafa76511eea21ad8a27932c9be9a8
Inosensey <dingcong.bae@gmail.com>
2025-01-16 06:36:36 +0800

    Added workouts page with layout and initial page files.

---
+++ app/dashboard/workouts/page.tsx
@@ -0,0 +1,13 @@
+"use server"
+
+import BodyTuneWorkouts from "@/components/dashboardComponents/bodytuneWorkouts/BodyTuneWorkouts"
+
+const BodyTuneWorkoutsPage = () => {
+  return (
+    <div className='px-4 mt-4 w-full'>
+      <BodyTuneWorkouts />
+    </div>
+  )
+}
+
+export default BodyTuneWorkoutsPage
