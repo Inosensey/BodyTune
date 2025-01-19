c72f42f8c16471f94b734f82acf980f51c90ce57
Inosensey <dingcong.bae@gmail.com>
2025-01-20 07:47:16 +0800

    Added meals page with initial layout and content.

---
+++ app/dashboard/meals/page.tsx
@@ -0,0 +1,13 @@
+"use server"
+
+import BodyTuneMeals from "@/components/dashboardComponents/bodytuneMeals/BodyTuneMeals"
+
+const BodyTuneMealsPage = () => {
+  return (
+    <div className='px-4 mt-4 w-full'>
+      <BodyTuneMeals />
+    </div>
+  )
+}
+
+export default BodyTuneMealsPage
