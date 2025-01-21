de5deadeff0635b5d86a552a5a28e1f349bee43f
Philip Mathew <dingcong.bae@gmail.com>
2025-01-21 14:54:39 +0800

    Created a new component folder called connections. This will hold all of the connection page related components.

---
+++ components/dashboardComponents/connections/ConnectionsContent.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const ConnectionsContent = () => {
+  return (
+    <div className="flex-1 bg-black w-full p-4 rounded-lg">
+      ConnectionsContent
+    </div>
+  );
+};
+
+export default ConnectionsContent;
