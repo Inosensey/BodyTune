a60d2c448b24d1aa58583739935486cbc8e61748
Inosensey <dingcong.bae@gmail.com>
2025-01-23 06:13:55 +0800

    Added three new icons for navigation and actions in the dashboard.

---
+++ icons/IcBaselineStarBorder.tsx
@@ -0,0 +1,25 @@
+import React, { SVGProps } from "react";
+
+type IcBaselineStarBorderProps = SVGProps<SVGSVGElement> & {
+  color: string;
+  width?: string;
+  height?: string;
+};
+
+export function IcBaselineStarBorder(props: IcBaselineStarBorderProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={props.width ? props.width : "1em"}
+      height={props.height ? props.height : "1em"}
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill={props.color}
+        d="m22 9.24l-7.19-.62L12 2L9.19 8.63L2 9.24l5.46 4.73L5.82 21L12 17.27L18.18 21l-1.63-7.03zM12 15.4l-3.76 2.27l1-4.28l-3.32-2.88l4.38-.38L12 6.1l1.71 4.04l4.38.38l-3.32 2.88l1 4.28z"
+      ></path>
+    </svg>
+  );
+}
+export default IcBaselineStarBorder;
