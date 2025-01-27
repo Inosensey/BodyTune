79ba57cc5839564831596b6d39012f7a1f2e5bf1
Philip Mathew <dingcong.bae@gmail.com>
2025-01-27 15:18:05 +0800

    Updated HTML structure and CSS in DashboardHeader.tsx to improve layout and styling.

--- components/dashboardComponents/DashboardHeader.tsx
+++ components/dashboardComponents/DashboardHeader.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRouter } from 'nextjs-toploader/app'
+import { useRouter } from "nextjs-toploader/app";
 
 // Types
 interface props {
@@ -14,19 +14,24 @@ const DashboardHeader = ({ headerText, headerDescription, Icon }: props) => {
 
   return (
     <div className="bg-black w-full p-4 rounded-lg">
-      <div className="flex flex-col gap-1">
-        <button onClick={() => router.back()} className="font-dmSans text-sm text-lightSecondary underline w-max cursor-pointer font-semibold">
+      <div className="flex gap-3">
+        <button
+          onClick={() => router.back()}
+          className="font-dmSans text-sm text-lightSecondary underline w-max cursor-pointer font-semibold"
+        >
           Back
         </button>
-        <div className="flex items-center gap-1">
-          <p className="text-lg font-dmSans font-bold text-lightSecondary">
-            {headerText}
+        <div className="flex flex-col">
+          <div className="flex items-center gap-1">
+            <p className="text-lg font-dmSans font-bold text-lightSecondary">
+              {headerText}
+            </p>
+            <Icon color="#D3F0D1" width="1.3em" height="1.3em" />
+          </div>
+          <p className="font-quickSand text-[0.8rem] text-[#b3b3b3]">
+            {headerDescription}
           </p>
-          <Icon color="#D3F0D1" width="1.3em" height="1.3em" />
         </div>
-        <p className="font-quickSand text-[0.8rem] text-[#b3b3b3]">
-          {headerDescription}
-        </p>
       </div>
     </div>
   );
