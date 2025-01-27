609e65815de74f6a41de275b3bccf486f07eb77b
Philip Mathew <dingcong.bae@gmail.com>
2025-01-27 15:16:35 +0800

    Improved CSS in Overview.tsx.

--- components/dashboardComponents/overviewComponents/Overview.tsx
+++ components/dashboardComponents/overviewComponents/Overview.tsx
@@ -8,13 +8,13 @@ import WeeklyActivity from "./WeeklyActivity";
 const Overview = () => {
   return (
     <div className="w-full h-full">
-      <div className="phone:w-11/12 laptop:max-w-[1200px]">
+      <div className="phone:w-full laptop:max-w-[1200px]">
         <WeeklyActivity />
       </div>
-      <div className="phone:w-11/12 laptop:max-w-[1200px]">
+      <div className="phone:w-full laptop:max-w-[1200px]">
         <Suggestions />
       </div>
-      <div className="phone:w-11/12 laptop:max-w-[1200px]">
+      <div className="phone:w-full laptop:max-w-[1200px]">
         <NewsFeed />
       </div>
     </div>
