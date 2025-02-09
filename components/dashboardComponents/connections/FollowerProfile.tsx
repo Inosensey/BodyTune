d7ca02ecdaed307c69de1315beec131b834761a8
Inosensey <dingcong.bae@gmail.com>
2025-02-10 06:33:14 +0800

    Improved CSS of FollowerProfile for responsiveness and optimized layout for small screen sizes.

--- components/dashboardComponents/connections/FollowerProfile.tsx
+++ components/dashboardComponents/connections/FollowerProfile.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 // Components
 import DashboardHeader from "../DashboardHeader";
@@ -10,11 +10,13 @@ import FollowerProfileContent from "./FollowerProfileContent";
 const FollowerProfile = () => {
   return (
     <div className="flex flex-col gap-3 h-[99%]">
-      <DashboardHeader
-        headerText="Connections"
-        headerDescription="Your Fitness Network"
-        Icon={IcBaselinePeopleOutline}
-      />
+      <div className="phone:px-4 laptop:px-0">
+        <DashboardHeader
+          headerText="Connections"
+          headerDescription="Your Fitness Network"
+          Icon={IcBaselinePeopleOutline}
+        />
+      </div>
       <FollowerProfileContent />
     </div>
   );
