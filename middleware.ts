1003350ac4fa7bbbd4889bc90e4222358aa994d6
Inosensey <dingcong.bae@gmail.com>
2025-02-13 18:59:22 +0800

    middleware updates: - Changed the condition that redirects the user if they are not logged in. - Created a function called checkIfUserHasPersonalInformation. - Removed the userHasPersonalInformation cookie and replaced it with the result of the checkIfUserHasPersonalInformation function. - Updated the config: added another path to run this middleware.

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,57 @@
 import { NextResponse } from "next/server";
-import { type NextRequest } from 'next/server'
-import { updateSession } from './utils/supabaseUpdateSession'
-import { cookies } from "next/headers";
+import { type NextRequest } from "next/server";
+import { updateSession } from "./utils/supabaseUpdateSession";
+import { createSSR } from "./utils/supabaseSSR";
 
 export async function middleware(request: NextRequest) {
+  const supabase = await createSSR()
+  const user = await supabase.auth.getUser();
   if (request.nextUrl.pathname.startsWith("/dashboard")) {
-    if (request.cookies.getAll().length === 0) {
-      return NextResponse.rewrite(new URL("/", request.url));
+    if (!user) {
+      return NextResponse.rewrite(new URL("/login", request.url));
     } else {
-      const cookieStore = await cookies()
-      const userHaverPersonalInformation = cookieStore.get('userHaverPersonalInformation')
-      if(userHaverPersonalInformation!.value === "false") {
+      const userHaverPersonalInformation = await checkIfUserHaverPersonalInformation(user.data.user!.id)
+      if(!userHaverPersonalInformation) {
         return NextResponse.rewrite(new URL("/profileSetup", request.url));
       }
+      // const cookieStore = await cookies();
+      // const userHaverPersonalInformation = cookieStore.get(
+      //   "userHaverPersonalInformation"
+      // );
+      // if (userHaverPersonalInformation!.value === "false") {
+      //   return NextResponse.rewrite(new URL("/profileSetup", request.url));
+      // }
     }
   }
   // update user's auth session
-  return await updateSession(request)
+  return await updateSession(request);
+}
+
+const checkIfUserHaverPersonalInformation = async (userId: string) => {
+  const supabase = await createSSR()
+  try {
+    const {data, error} = await supabase.from("personal_information").select("id").eq("user_id", userId)
+    
+    if (error) {
+      const errorMessage: string = `There is an error getting your personal Information: ${error.message}`;
+      return {
+        message: errorMessage,
+      };
+    }
+    if(data.length === 1) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error: unknown) {
+    const errorMessage: string =
+      error instanceof Error
+        ? `There is an error getting your personal information: ${error.message}`
+        : "An unknown error occurred";
+    return {
+      message: errorMessage,
+    };
+  }
 }
 
 export const config = {
@@ -28,6 +63,8 @@ export const config = {
      * - favicon.ico (favicon file)
      * Feel free to modify this pattern to include more paths.
      */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/dashboard/:path*",
+    "/dashboard",
   ],
-}
+};
