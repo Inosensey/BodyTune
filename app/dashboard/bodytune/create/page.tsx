6334618970e442cad20d9838ac4410664975e99d
Philip Mathew <dingcong.bae@gmail.com>
2025-02-14 10:29:20 +0800

    Refactored CreateBodyTune page to use getUserInformation for retrieving personal information.

--- app/dashboard/bodytune/create/page.tsx
+++ app/dashboard/bodytune/create/page.tsx
@@ -1,8 +1,6 @@
 "use server";
 
-import { headers } from "next/headers";
-import getUser from "@/lib/getUser";
-import { encryptUserId } from "@/utils/encrypter";
+import getUserInformation from "@/lib/getUserInformation";
 
 // Components
 import MutateForm from "@/components/dashboardComponents/bodytuneStudio/MutateComponents/MutateForm";
@@ -11,19 +9,18 @@ import MutateForm from "@/components/dashboardComponents/bodytuneStudio/MutateCo
 import { TableRow } from "@/types/database.types";
 
 const CreateBodyTunePage = async () => {
-  const user = await getUser();
-  const userId = user.data.user!.id;
-  const encryptedUserId = encryptUserId(userId);
-  const headerInfo = headers();
-  const res = await fetch(
-    `http://localhost:3000/api/supabase/getUserInformation?user=${encryptedUserId}`,
-    {
-      headers: { cookie: headerInfo.get("cookie")! },
-      next: { tags: ["personalInformation"] },
-      cache: "force-cache",
-    }
-  );
-  const personalInformation: { response: TableRow<"personal_information">[] } = await res.json();
+  const res = await getUserInformation();
+  let userInformation:
+    | { response: TableRow<"personal_information">[] }
+    | { response: [] } = { response: [] };
+  if (res) {
+    userInformation = await res.json();
+  } else {
+    userInformation  = { response: [] };
+  }
+  const personalInformation:
+    | { response: TableRow<"personal_information">[] }
+    | [] = userInformation;
 
   return (
     <div className="px-4 mt-4 w-full">
