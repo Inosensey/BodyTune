417bbd5e2e605518db1ca98a4bcce5fd1469d89a
Philip Mathew <dingcong.bae@gmail.com>
2025-02-14 10:29:33 +0800

    Refactored CreateMeal page to use getUserInformation for retrieving personal information.

--- app/dashboard/meals/create/page.tsx
+++ app/dashboard/meals/create/page.tsx
@@ -1,27 +1,24 @@
 "use server";
 
-import { headers } from "next/headers";
-import getUser from "@/lib/getUser";
-import { encryptUserId } from "@/utils/encrypter";
+import getUserInformation from "@/lib/getUserInformation";
 
 // Types
 import { TableRow } from "@/types/database.types";
 import MutateForm from "@/components/dashboardComponents/bodytuneMeals/Mutatecomponents/MutateForm";
 
 const CreateMealPage = async () => {
-    const user = await getUser();
-    const userId = user.data.user!.id;
-    const encryptedUserId = encryptUserId(userId);
-    const headerInfo = headers();
-    const res = await fetch(
-      `http://localhost:3000/api/supabase/getUserInformation?user=${encryptedUserId}`,
-      {
-        headers: { cookie: headerInfo.get("cookie")! },
-        next: { tags: ["personalInformation"] },
-        cache: "force-cache",
-      }
-    );
-    const personalInformation: { response: TableRow<"personal_information">[] } = await res.json();
+  const res = await getUserInformation();
+  let userInformation:
+    | { response: TableRow<"personal_information">[] }
+    | { response: [] } = { response: [] };
+  if (res) {
+    userInformation = await res.json();
+  } else {
+    userInformation  = { response: [] };
+  }
+  const personalInformation:
+    | { response: TableRow<"personal_information">[] }
+    | [] = userInformation;
   return (
     
     <div className="px-4 mt-4 w-full"><MutateForm personalInfo={personalInformation.response} /></div>
