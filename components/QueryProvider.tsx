44517bac18f24285f2f821e7e8f0993069c0b506
Philip Mathew <dingcong.bae@gmail.com>
2025-02-21 16:00:04 +0800

    Created QueryProvider component to wrap layouts for TanStack React Query.

---
+++ components/QueryProvider.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { ReactNode, useState } from "react";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const QueryProvider = ({ children }: { children: ReactNode }) => {
+  const [queryClient] = useState(() => new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000
+      }
+    }
+  }));
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      {children}
+    </QueryClientProvider>
+  );
+};
+
+export default QueryProvider;
