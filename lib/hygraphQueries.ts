d21c78aeaf2bcc4cd01d9d2ddf44e6782d5bfc55
Philip Mathew <dingcong.bae@gmail.com>
2025-02-28 15:43:35 +0800

    hygraphQueries Updates: - Added setMealPlan function to generate a meal plan from provided meals. - Added setExercisePlan function to generate an exercise plan from provided exercises.

--- lib/hygraphQueries.ts
+++ lib/hygraphQueries.ts
@@ -1,5 +1,9 @@
 "use server";
 
+import { exercisePlan, exerciseQueryHygraphType, mealPlan, mealQueryHygraphType } from "@/types/planTypes";
+
+import { days } from "@/utils/date";
+
 const NEXT_HYGRAPH_ENDPOINT = process.env.NEXT_HYGRAPH_ENDPOINT;
 
 export const getMeals = async () => {
@@ -11,7 +15,7 @@ export const getMeals = async () => {
       },
       body: JSON.stringify({
         query: `query Meals {
-                    meals(first: 25) {
+                    meals(first: 100) {
                         mealType,
                         mealName,
                         ingredients,
@@ -39,7 +43,7 @@ export const getWorkouts = async () => {
       body: JSON.stringify({
         query: `query Exercises {
                         exercises(
-                        first: 25
+                        first: 100
                     ) {
                         day,
                         exerciseName,
@@ -85,7 +89,7 @@ const getMealsByBmi = async (bmiClassification: string) => {
       },
       body: JSON.stringify({
         query: `query Meals {
-                    meals(first: 25, where: {bmiClassification:{id: "${bmiClassification}"}}) {
+                    meals(first: 100, where: {bmiClassification:{id: "${bmiClassification}"}}) {
                         mealType,
                         mealName,
                         ingredients,
@@ -117,7 +121,7 @@ const getExerciseByBmi = async (bmiClassification: string) => {
       body: JSON.stringify({
         query: `query Exercises {
                         exercises(
-                        first: 25, 
+                        first: 100, 
                         where: { bmiClassification: { id: "${bmiClassification}" } }
                     ) {
                         day,
@@ -144,7 +148,7 @@ const getExerciseByBmi = async (bmiClassification: string) => {
       }),
     });
     const json = await response.json();
-    return json;
+    return json;  
   } catch (error) {
     console.log("There is an error getting your Exercises", error);
     return error;
@@ -154,7 +158,51 @@ const getExerciseByBmi = async (bmiClassification: string) => {
 export const generateBodyTunePlan = async (bmiClassification: string) => {
   const mealList = await getMealsByBmi(bmiClassification);
   const exerciseList = await getExerciseByBmi(bmiClassification);
-  console.log(mealList);
-  console.log(exerciseList);
-  return { mealList, exerciseList };
+  const mealPlan: mealPlan = setMealPlan(mealList.data.meals)
+  const exercisePlan: exercisePlan = setExercisePlan(exerciseList.data.exercises)
+  console.log(exercisePlan)
+  return { mealPlan, exercisePlan };
+};
+
+const setMealPlan = (meals:Array<mealQueryHygraphType>) => {
+  const shuffledMeals = [...meals].sort(() => Math.random() - 0.5);
+  const mealPlan: mealPlan = {};
+  let mealIndex = 0;
+
+  days.forEach(day => {
+      if (mealIndex + 3 > shuffledMeals.length) return;
+
+      mealPlan[day] = {
+          Breakfast: shuffledMeals[mealIndex++],
+          Lunch: shuffledMeals[mealIndex++],
+          Dinner: shuffledMeals[mealIndex++]
+      };
+  });
+
+  return mealPlan;
+}
+
+const setExercisePlan = (exercises: Array<exerciseQueryHygraphType>) => {
+  const exercisePlan: exercisePlan = {};
+
+  days.forEach(day => {
+    const filteredExercises = exercises
+      .filter(exercise => exercise.day === day)
+      .map(exercise => ({
+        exerciseName: exercise.exerciseName,
+        bodyPart: exercise.bodyPart,
+        equipment: exercise.equipment,
+        exerciseDifficulty: 1,
+        exerciseMeasurementType: 1,
+        measurement: exercise.measurement,
+        exerciseDemo: exercise.exerciseDemo?.url,
+        bmiClassification: 1,
+        instruction: exercise.instruction,
+        youtubeLink: exercise?.youtubeLink
+      }));
+
+    exercisePlan[day] = filteredExercises.length > 0 ? filteredExercises : "Rest Day";
+  });
+
+  return exercisePlan;
 };
