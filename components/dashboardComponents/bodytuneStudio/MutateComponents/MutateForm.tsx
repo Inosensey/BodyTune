cc6c7cfbe7d15b4e02b027f0cad94b3b379b6a80
Philip Mathew <dingcong.bae@gmail.com>
2025-03-05 15:56:53 +0800

    MutateForm Updates: - Imported useFormState. - Imported createBodyTunePlan from planActions. - Imported the LoadingPopUp component. - Imported ormReturnType type. - Created two new types: mealPlanInterface and exercisePlanInterface. - Created three new initial states: useFormStateInitials, mealPlanFieldsInit, and exercisePlanInitials. - Created four new states: isSubmitting, submitMessage, mealPlanFieldsVal, and exercisePlanFieldsVal. - Created a useFormState. - Created a handleSubmit function. - Added a new useEffect that manages form submission status. - Wrapped the entire HTML in a Fragment. - Wrapped the steps in a <form> with ormAction as the action value and handleSubmit as the onSubmit handler. - Passed mealPlanFieldsVal and setMealPlanFieldsVal as props to SetMealPlan. - Passed exercisePlanFieldsVal and setExercisePlanFieldsVal as props to SetExercisePlan. - Rendered the LoadingPopUp component with submitMessage and isSubmitting as props.

--- components/dashboardComponents/bodytuneStudio/MutateComponents/MutateForm.tsx
+++ components/dashboardComponents/bodytuneStudio/MutateComponents/MutateForm.tsx
@@ -1,6 +1,11 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { useFormState } from "react-dom";
+
+// Actions
+import { createBodyTunePlan } from "@/actions/planActions";
+import LoadingPopUp from "@/components/reusableComponent/loadingAnimation/LoadingPopUp";
 
 // Components
 import DashboardHeader from "@/components/dashboardComponents/DashboardHeader";
@@ -19,6 +24,7 @@ import { TableRow } from "@/types/database.types";
 import { InterfaceBreadCrumbs } from "@/types/inputTypes";
 import { exercisePlan } from "@/types/planTypes";
 import { mealPlanType } from "@/types/mealTypes";
+import { formReturnType } from "@/types/formTypes";
 interface props {
   personalInfo: TableRow<"personal_information">[];
 }
@@ -28,8 +34,30 @@ interface generalInfoType {
   experience: string;
   bmi: string;
 }
+interface mealPlanInterface {
+  selectedMealPlan: string;
+  mealPlanName: string;
+}
+interface exercisePlanInterface {
+  selectedExercisePlan: number;
+  exercisePlanName: string;
+}
 
 // Initials
+const useFormStateInitials: formReturnType<[]> = {
+  success: null,
+  error: null,
+  message: "",
+  data: [],
+};
+const mealPlanFieldsInit: mealPlanInterface = {
+  selectedMealPlan: "0",
+  mealPlanName: "",
+};
+const exercisePlanInitials: exercisePlanInterface = {
+  selectedExercisePlan: 0,
+  exercisePlanName: "",
+};
 const BreadCrumbsInitials: InterfaceBreadCrumbs[] = [
   {
     id: 1,
@@ -73,27 +101,34 @@ const mealPlanInitial: mealPlanType = {
   },
 };
 
-const exercisePlanInitial: exercisePlan = {
-  ["Monday"]: [
-    {
-      exerciseName: "",
-      bodyPart: "",
-      equipment: "",
-      day: "",
-      exerciseDifficulty: 1,
-      exerciseMeasurementType: 1,
-      measurement: "",
-      exerciseDemo: "",
-      bmiClassification: 1,
-      instruction: "",
-      youtubeLink: "",
-    },
-  ],
-};
+// const exercisePlanInitial: exercisePlan = {
+//   ["Monday"]: [
+//     {
+//       exerciseName: "",
+//       bodyPart: "",
+//       equipment: "",
+//       day: "",
+//       exerciseDifficulty: 1,
+//       exerciseMeasurementType: 1,
+//       measurement: "",
+//       exerciseDemo: "",
+//       bmiClassification: 1,
+//       instruction: "",
+//       youtubeLink: "",
+//     },
+//   ],
+// };
 
 const MutateForm = ({ personalInfo }: props) => {
+  // UseFormState
+  const [formState, formAction] = useFormState(
+    createBodyTunePlan,
+    useFormStateInitials
+  );
 
   // States
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+  const [submitMessage, setSubmitMessage] = useState<string>("");
   const [generalInfoFieldsVal, setGeneralInfoFieldsVal] =
     useState<generalInfoType>({
       height: personalInfo[0].height!.toString(),
@@ -101,6 +136,10 @@ const MutateForm = ({ personalInfo }: props) => {
       experience: "",
       bmi: "",
     });
+  const [mealPlanFieldsVal, setMealPlanFieldsVal] =
+    useState<mealPlanInterface>(mealPlanFieldsInit);
+  const [exercisePlanFieldsVal, setExercisePlanFieldsVal] =
+    useState<exercisePlanInterface>(exercisePlanInitials);
   const [selectedOption, setSelectedOption] = useState<string>("");
   const [selectedBreadCrumb, setSelectedBreadCrumb] =
     useState<InterfaceBreadCrumbs>({
@@ -111,100 +150,129 @@ const MutateForm = ({ personalInfo }: props) => {
   const [progress, setProgress] = useState<number>(1);
   const [mealPlanInfo, setMealPlanInfo] =
     useState<mealPlanType>(mealPlanInitial);
-  const [exercisePlanInfo, setExercisePlanInfo] = useState<exercisePlan>(exercisePlanInitial);
+  const [exercisePlanInfo, setExercisePlanInfo] = useState<exercisePlan>({});
   const [disabledBreadCrumbs, setDisabledBreadCrumbs] = useState<number[]>([]);
 
+  // Events
+  const handleSubmit = () => {
+    setSubmitMessage("test");
+    setIsSubmitting(true);
+  };
+
+  // useEffect
   useEffect(() => {
-    if(selectedOption === "") return 
-    if(selectedOption === "recommendation") {
-      setDisabledBreadCrumbs([2, 3, 4])
+    if (selectedOption === "") return;
+    if (selectedOption === "recommendation") {
+      setDisabledBreadCrumbs([2, 3, 4]);
     } else {
-      setDisabledBreadCrumbs([])
+      setDisabledBreadCrumbs([]);
     }
-  },[selectedOption])
+  }, [selectedOption]);
+  useEffect(() => {
+    if (formState.success !== null || formState.error !== null) {
+      if (formState.success) {
+        console.log(formState);
+        // setSubmitMessage("You're in! 🎯 Taking you to your dashboard—let’s crush some goals today! 💪");
+        setIsSubmitting(false);
+      } else {
+        setIsSubmitting(false);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [formState]);
 
   return (
-    <div className="flex flex-col gap-2 h-[99%] relative">
-      <div className="phone:h-[17%] laptop:h-[12%]">
-        <DashboardHeader
-          headerText="Create Your BodyTune"
-          headerDescription="Craft a personalized plan by combining workouts and meals into your perfect routine."
-          Icon={SolarStarsMinimalisticLineDuotone}
-        />
-      </div>
-      <div className="h-[83%] flex flex-col flex-1 gap-1 items-center">
-        {selectedOption === "" ? (
-          <CreationOption
-            label="How would you like to create your BodyTune"
-            setSelectOption={setSelectedOption}
+    <>
+      <div className="flex flex-col gap-2 h-[99%] relative">
+        <div className="phone:h-[17%] laptop:h-[12%]">
+          <DashboardHeader
+            headerText="Create Your BodyTune"
+            headerDescription="Craft a personalized plan by combining workouts and meals into your perfect routine."
+            Icon={SolarStarsMinimalisticLineDuotone}
           />
-        ) : (
-          <div className="flex flex-1 h-full flex-col items-center justify-center-center gap-1 w-full">
-            <div className="h-max max-w-[800px] mb-1">
-              <BreadCrumbs
-                breadCrumbs={BreadCrumbsInitials}
-                setSelectedBreadCrumb={setSelectedBreadCrumb}
-                setProgress={setProgress}
-                selectedBreadCrumb={selectedBreadCrumb}
-                selectedCreateOption={selectedOption}
-                disabledBreadCrumbs={disabledBreadCrumbs}
-              />
-            </div>
+        </div>
+        <div className="h-[83%] flex flex-col flex-1 gap-1 items-center">
+          {selectedOption === "" ? (
+            <CreationOption
+              label="How would you like to create your BodyTune"
+              setSelectOption={setSelectedOption}
+            />
+          ) : (
+            <div className="flex flex-1 h-full flex-col items-center justify-center-center gap-1 w-full">
+              <div className="h-max max-w-[800px] mb-1">
+                <BreadCrumbs
+                  breadCrumbs={BreadCrumbsInitials}
+                  setSelectedBreadCrumb={setSelectedBreadCrumb}
+                  setProgress={setProgress}
+                  selectedBreadCrumb={selectedBreadCrumb}
+                  selectedCreateOption={selectedOption}
+                  disabledBreadCrumbs={disabledBreadCrumbs}
+                />
+              </div>
 
-            {progress === 1 && (
-              <div className="flex justify-center max-h w-full">
-                <SetGeneralInfo
-                  personalInfo={personalInfo[0]}
-                  setSelectedOption={setSelectedOption}
-                  setDisabledBreadCrumbs={setDisabledBreadCrumbs}
-                  setProgress={setProgress}
-                  setSelectedBreadCrumb={setSelectedBreadCrumb}
-                  selectedCreateOption={selectedOption}
-                  generalInfoFieldsVal={generalInfoFieldsVal}
-                  setGeneralInfoFieldsVal={setGeneralInfoFieldsVal}
-                  setExercisePlanInfo={setExercisePlanInfo}
-                  setMealPlanInfo={setMealPlanInfo}
-                />
-              </div>
-            )}
-            {progress === 2 && (
-              <div className="flex flex-1 justify-center h-[75%] phone:w-full laptop:w-[90%]">
-                <SetMealPlan
-                  setSelectedOption={setSelectedOption}
-                  setSelectedBreadCrumb={setSelectedBreadCrumb}
-                  setProgress={setProgress}
-                  mealPlanInfo={mealPlanInfo}
-                  setMealPlanInfo={setMealPlanInfo}
-                  selectedCreateOption={selectedOption}
-                />
-              </div>
-            )}
-            {progress === 3 && (
-              <div className="flex flex-1 justify-center h-[75%]">
-                <SetExercisePlan
-                  setSelectedOption={setSelectedOption}
-                  setSelectedBreadCrumb={setSelectedBreadCrumb}
-                  setProgress={setProgress}
-                  exercisePlanInfo={exercisePlanInfo}
-                  setExercisePlanInfo={setExercisePlanInfo}
-                  selectedCreateOption={selectedOption}
-                />
-              </div>
-            )}
-            {progress === 4 && (
-              <div className="flex flex-1 justify-center h-[75%] w-full">
-                <SetVisibility
-                  setSelectedOption={setSelectedOption}
-                  setSelectedBreadCrumb={setSelectedBreadCrumb}
-                  setProgress={setProgress}
-                  Icon={SolarStarsMinimalisticLineDuotone}
-                />
-              </div>
-            )}
-          </div>
-        )}
+              <form action={formAction} onSubmit={handleSubmit}>
+                {progress === 1 && (
+                  <div className="flex justify-center max-h w-full">
+                    <SetGeneralInfo
+                      personalInfo={personalInfo[0]}
+                      setSelectedOption={setSelectedOption}
+                      setDisabledBreadCrumbs={setDisabledBreadCrumbs}
+                      setProgress={setProgress}
+                      setSelectedBreadCrumb={setSelectedBreadCrumb}
+                      selectedCreateOption={selectedOption}
+                      generalInfoFieldsVal={generalInfoFieldsVal}
+                      setGeneralInfoFieldsVal={setGeneralInfoFieldsVal}
+                      setExercisePlanInfo={setExercisePlanInfo}
+                      setMealPlanInfo={setMealPlanInfo}
+                    />
+                  </div>
+                )}
+                {progress === 2 && (
+                  <div className="flex flex-1 justify-center h-[75%] phone:w-full laptop:w-[90%]">
+                    <SetMealPlan
+                      mealPlanFieldsVal={mealPlanFieldsVal}
+                      setMealPlanFieldsVal={setMealPlanFieldsVal}
+                      setSelectedOption={setSelectedOption}
+                      setSelectedBreadCrumb={setSelectedBreadCrumb}
+                      setProgress={setProgress}
+                      mealPlanInfo={mealPlanInfo}
+                      setMealPlanInfo={setMealPlanInfo}
+                      selectedCreateOption={selectedOption}
+                    />
+                  </div>
+                )}
+                {progress === 3 && (
+                  <div className="flex flex-1 justify-center h-[75%]">
+                    <SetExercisePlan
+                      exercisePlanFieldsVal={exercisePlanFieldsVal}
+                      setExercisePlanFieldsVal={setExercisePlanFieldsVal}
+                      setSelectedOption={setSelectedOption}
+                      setSelectedBreadCrumb={setSelectedBreadCrumb}
+                      setProgress={setProgress}
+                      exercisePlanInfo={exercisePlanInfo}
+                      setExercisePlanInfo={setExercisePlanInfo}
+                      selectedCreateOption={selectedOption}
+                    />
+                  </div>
+                )}
+                {progress === 4 && (
+                  <div className="flex flex-1 justify-center h-[75%] w-full">
+                    <SetVisibility
+                      setSelectedOption={setSelectedOption}
+                      setSelectedBreadCrumb={setSelectedBreadCrumb}
+                      setProgress={setProgress}
+                      Icon={SolarStarsMinimalisticLineDuotone}
+                    />
+                  </div>
+                )}
+              </form>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+
+      <LoadingPopUp message={submitMessage} isLoading={isSubmitting} />
+    </>
   );
 };
 
