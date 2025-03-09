a9f82cd3db2a3db9b0a231198998bcf6219c99fd
Inosensey <dingcong.bae@gmail.com>
2025-03-09 22:52:02 +0800

    dashboardUtils Updates: - Imported the newly added initials. - Created a function called generateMealPlanName to auto-generate a meal plan name. - Created a function called generateExercisePlanName to auto-generate an exercise plan name.

--- utils/dashboardUtils.ts
+++ utils/dashboardUtils.ts
@@ -1,3 +1,5 @@
+import {underweightMealPlanNames, healthyWeightMealPlanNames, overweightMealPlanNames, beginnerExercisePlanNames, amateurExercisePlanNames, expertExercisePlanNames} from "@/utils/initials";
+
 export const getBmi = (weight: number, height: number): {
     id: string,
     bmiClassification: string,
@@ -19,4 +21,52 @@ export const getBmi = (weight: number, height: number): {
             bmiClassification: "Overweight",    
         };
     }
+}
+
+export const generateMealPlanName = (bmiClassifications:string):string => {
+    let planName:string = "";
+    let randomIndex: number;
+    switch (bmiClassifications) {
+        case "Underweight":
+            randomIndex = Math.floor(Math.random() * underweightMealPlanNames.length)
+            planName = underweightMealPlanNames[randomIndex];
+            break;
+        case "Healthy weight":
+            randomIndex = Math.floor(Math.random() * underweightMealPlanNames.length)
+            planName = healthyWeightMealPlanNames[randomIndex];
+            break;
+        case "Overweight":
+            randomIndex = Math.floor(Math.random() * underweightMealPlanNames.length)
+            planName = overweightMealPlanNames[randomIndex];
+            break;
+        default:
+            randomIndex = Math.floor(Math.random() * underweightMealPlanNames.length)
+            planName = healthyWeightMealPlanNames[randomIndex];
+            break;
+    }
+    return planName;
+}
+
+export const generateExercisePlanName = (difficulty:string):string => {
+    let planName:string = "";
+    let randomIndex: number;
+    switch (difficulty) {
+        case "beginner":
+            randomIndex = Math.floor(Math.random() * underweightMealPlanNames.length)
+            planName = beginnerExercisePlanNames[randomIndex];
+            break;
+        case "Amateur":
+            randomIndex = Math.floor(Math.random() * underweightMealPlanNames.length)
+            planName = amateurExercisePlanNames[randomIndex];
+            break;
+        case "Expert":
+            randomIndex = Math.floor(Math.random() * underweightMealPlanNames.length)
+            planName = expertExercisePlanNames[randomIndex];
+            break;
+        default:
+            randomIndex = Math.floor(Math.random() * underweightMealPlanNames.length)
+            planName = amateurExercisePlanNames[randomIndex];
+            break;
+    }
+    return planName;
 }
