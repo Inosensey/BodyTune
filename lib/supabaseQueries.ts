ba3ba18a833999bfff9e8400abe755c29e98103f
Inosensey <dingcong.bae@gmail.com>
2025-03-16 22:02:58 +0800

    Added supabaseQueries.ts with an initial function getBodyTunes.

---
+++ lib/supabaseQueries.ts
@@ -0,0 +1,31 @@
+"use server"
+
+import { allowedOrigins } from "@/utils/initials";
+import { headers } from "next/headers";
+
+// Types
+import { bodyTunePlan } from "@/types/planTypes";
+
+export const getBodyTunes = async () => {
+    const headerInfo = headers();
+    
+    const host = headerInfo.get('X-Forwarded-Host');
+    const proto = headerInfo.get('X-Forwarded-Proto');
+    const origin = `${proto}://${host}`;
+
+    if (origin && allowedOrigins.includes(origin)) {
+        const res = await fetch(
+            `${origin}/api/supabase/getBodyTunes`,
+            {
+                headers: { cookie: headerInfo.get("cookie")! },
+                next: { tags: ["bodyTunes"] },
+                cache: "force-cache",
+            }
+        );
+        const parsedData = await res.json();
+        const bodyTunes: Array<bodyTunePlan> | undefined = parsedData.data;
+        return bodyTunes
+    } else {
+        return undefined;
+    }
+}
