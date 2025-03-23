ad77439c1c4956de7ea59a59ea7a843c616ae843
Inosensey <dingcong.bae@gmail.com>
2025-03-24 06:24:33 +0800

    Added another path to the config in middleware.ts.

--- middleware.ts
+++ middleware.ts
@@ -71,5 +71,7 @@ export const config = {
     "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
     "/dashboard/:path*",
     "/dashboard",
+    "/plan",
+    "/plan/:path*",
   ],
 };
