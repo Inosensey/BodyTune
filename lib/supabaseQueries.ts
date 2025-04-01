c1f38cb521d1ad49d4e4d26fb088b5f256a2f07b
Inosensey <dingcong.bae@gmail.com>
2025-04-01 22:02:54 +0800

    supabaseQueries Updates: - Added two more functions called getMealPlans and getExercisePlans. - Small adjustments to the indentations.

--- lib/supabaseQueries.ts
+++ lib/supabaseQueries.ts
@@ -1,55 +1,97 @@
-"use server"
+"use server";
 
 import { allowedOrigins } from "@/utils/initials";
 import { headers } from "next/headers";
 
 // Types
-import { bodyTunePlan } from "@/types/planTypes";
+import {
+  bodyTunePlan,
+  exercisePlanQuery,
+  mealPlanQuery,
+} from "@/types/planTypes";
 
 export const getBodyTunes = async () => {
-    const headerInfo = headers();
-    
-    const host = headerInfo.get('X-Forwarded-Host');
-    const proto = headerInfo.get('X-Forwarded-Proto');
-    const origin = `${proto}://${host}`;
+  const headerInfo = headers();
 
-    if (origin && allowedOrigins.includes(origin)) {
-        const res = await fetch(
-            `${origin}/api/supabase/getBodyTunes`,
-            {
-                headers: { cookie: headerInfo.get("cookie")! },
-                next: { tags: ["bodyTunes"] },
-                cache: "force-cache",
-            }
-        );
-        const parsedData = await res.json();
-        const bodyTunes: Array<bodyTunePlan> | undefined = parsedData.res;
-        return bodyTunes
-    } else {
-        return [];
-    }
-}
+  const host = headerInfo.get("X-Forwarded-Host");
+  const proto = headerInfo.get("X-Forwarded-Proto");
+  const origin = `${proto}://${host}`;
+
+  if (origin && allowedOrigins.includes(origin)) {
+    const res = await fetch(`${origin}/api/supabase/getBodyTunes`, {
+      headers: { cookie: headerInfo.get("cookie")! },
+      next: { tags: ["bodyTunes"] },
+      cache: "force-cache",
+    });
+    const parsedData = await res.json();
+    const bodyTunes: Array<bodyTunePlan> | undefined = parsedData.res;
+    return bodyTunes;
+  } else {
+    return [];
+  }
+};
 
 export const getBodyTune = async (planId: number) => {
-    const headerInfo = headers();
-    
-    const host = headerInfo.get('X-Forwarded-Host');
-    const proto = headerInfo.get('X-Forwarded-Proto');
-    const origin = `${proto}://${host}`;
+  const headerInfo = headers();
 
-    if (origin && allowedOrigins.includes(origin)) {
-        const res = await fetch(
-            `${origin}/api/supabase/getBodyTune?planId=${planId}`,
-            {
-                headers: { cookie: headerInfo.get("cookie")! },
-                next: { tags: [`bodyTunes${planId}`] },
-                cache: "force-cache",
-            }
-        );
-        const parsedData = await res.json();
-        const bodyTune: Array<bodyTunePlan> | [] = parsedData.res;
-        return bodyTune
-    } else {
-        return [];
-    }
-}
+  const host = headerInfo.get("X-Forwarded-Host");
+  const proto = headerInfo.get("X-Forwarded-Proto");
+  const origin = `${proto}://${host}`;
+
+  if (origin && allowedOrigins.includes(origin)) {
+    const res = await fetch(
+      `${origin}/api/supabase/getBodyTune?planId=${planId}`,
+      {
+        headers: { cookie: headerInfo.get("cookie")! },
+        next: { tags: [`bodyTunes${planId}`] },
+        cache: "force-cache",
+      }
+    );
+    const parsedData = await res.json();
+    const bodyTune: Array<bodyTunePlan> | [] = parsedData.res;
+    return bodyTune;
+  } else {
+    return [];
+  }
+};
+
+export const getMealPlans = async () => {
+  const headerInfo = headers();
+
+  const host = headerInfo.get("X-Forwarded-Host");
+  const proto = headerInfo.get("X-Forwarded-Proto");
+  const origin = `${proto}://${host}`;
+
+  if (origin && allowedOrigins.includes(origin)) {
+    const res = await fetch(`${origin}/api/supabase/getMealPlans`, {
+      headers: { cookie: headerInfo.get("cookie")! },
+      next: { tags: [`mealPlans`] },
+      cache: "force-cache",
+    });
+    const parsedData = await res.json();
+    const mealPlans: Array<mealPlanQuery> | [] = parsedData.res;
+    return mealPlans;
+  } else {
+    return [];
+  }
+};
+export const getExercisePlans = async () => {
+  const headerInfo = headers();
+
+  const host = headerInfo.get("X-Forwarded-Host");
+  const proto = headerInfo.get("X-Forwarded-Proto");
+  const origin = `${proto}://${host}`;
+
+  if (origin && allowedOrigins.includes(origin)) {
+    const res = await fetch(`${origin}/api/supabase/getExercisePlans`, {
+      headers: { cookie: headerInfo.get("cookie")! },
+      next: { tags: [`exercisePlans`] },
+      cache: "force-cache",
+    });
+    const parsedData = await res.json();
+    const exercisePlans: Array<exercisePlanQuery> | [] = parsedData.res;
+    return exercisePlans;
+  } else {
+    return [];
+  }
+};
