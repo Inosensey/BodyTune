e8cc31e8b0822e9e8a7e93b60693a428a5c8ac86
Inosensey <dingcong.bae@gmail.com>
2025-04-01 22:02:41 +0800

    planTypes Updates: - Added four more types called mealPlanQuery, exercisePlanQuery, exercisePlanInfo, and mealPlanInfo. - Updated the odyTunePlan type.

--- types/planTypes.ts
+++ types/planTypes.ts
@@ -1,5 +1,4 @@
 import { TableInsert } from "@/types/database.types";
-// import { mealPlanType } from "./mealTypes";
 
 export interface exerciseQueryHygraphType {
   day: string;
@@ -39,126 +38,153 @@ export interface exercisePlan {
 }
 
 export interface bodyTunePlan {
+  id: number,
   mealPlanId: number;
   exercisePlanId: number;
   created_by: string;
   personal_information: {
     name: string;
   };
-  meal_plan: {
-    id: number;
-    planName: string;
-    created_by: string;
-    visibility: number;
-    meal_plan_tags: Array<{
-      meal_tags: {
-        id: number;
-        mealTagName: string;
-      };
-    }>;
-    plan_visibility: {
-      visibility: string;
+  meal_plan: mealPlanQuery;
+  exercise_plan: exercisePlanQuery;
+}
+
+export interface exercisePlanQuery {
+  id: number;
+  planName: string;
+  created_by: string;
+  visibility: number;
+  exercise_plan_tag: Array<{
+    exercise_tags: {
+      id: number;
+      exerciseTagName: string;
     };
-    personal_information: {
-      name: string;
-    };
-    daily_meals: Array<{
-      plan_id: number;
+  }>;
+  plan_visibility: {
+    visibility: string;
+  };
+  personal_information: {
+    name: string;
+  };
+  exercise: [
+    {
+      exerciseName: string;
+      bodyPart: string;
+      equipment: string;
       day: string;
-      breakFast: {
+      exerciseDemo: string;
+      youtubeLink: string | null;
+      measurement: string;
+      instruction: string;
+      bmiClassification: number;
+      exerciseMeasurementType: number;
+      exercise_measurement_type: {
         id: number;
-        mealName: string;
-        mealType: {
-          mealType: string;
-        };
-        instructions: string;
-        meal_ingredients: Array<{
+        measurement: string;
+      };
+      bmi_classification: {
+        classification: string;
+      };
+    }
+  ];
+}
+
+export interface mealPlanQuery {
+  id: number;
+  planName: string;
+  created_by: string;
+  visibility: number;
+  meal_plan_tags: Array<{
+    meal_tags: {
+      id: number;
+      mealTagName: string;
+    };
+  }>;
+  plan_visibility: {
+    visibility: string;
+  };
+  personal_information: {
+    name: string;
+  };
+  daily_meals: Array<{
+    plan_id: number;
+    day: string;
+    breakFast: {
+      id: number;
+      mealName: string;
+      mealType: {
+        mealType: string;
+      };
+      instructions: string;
+      meal_ingredients: Array<{
+        mealId: number;
+        ingredientName: string;
+        fat: number;
+        carbs: number;
+        protein: number;
+        calories: number;
+      }>;
+      veganAlternative: string | null;
+    };
+    lunch: {
+      id: number;
+      mealName: string;
+      mealType: {
+        mealType: string;
+      };
+      instructions: string;
+      meal_ingredients: [
+        {
           mealId: number;
           ingredientName: string;
           fat: number;
           carbs: number;
           protein: number;
           calories: number;
-        }>;
-        veganAlternative: string | null;
-      };
-      lunch: {
-        id: number;
-        mealName: string;
-        mealType: {
-          mealType: string;
-        };
-        instructions: string;
-        meal_ingredients: [
-          {
-            mealId: number;
-            ingredientName: string;
-            fat: number;
-            carbs: number;
-            protein: number;
-            calories: number;
-          }
-        ];
-        veganAlternative: string | null;
-      };
-      dinner: {
-        id: number;
-        mealName: string;
-        mealType: {
-          mealType: string;
-        };
-        instructions: string;
-        meal_ingredients: [
-          {
-            mealId: number;
-            ingredientName: string;
-            fat: number;
-            carbs: number;
-            protein: number;
-            calories: number;
-          }
-        ];
-        veganAlternative: string | null;
-      };
-    }>;
-  };
-  exercise_plan: {
-    id: number;
-    planName: string;
-    created_by: string;
-    visibility: number;
-    exercise_plan_tag: Array<{
-      exercise_tags: {
-        id: number;
-        exerciseTagName: string;
-      };
-    }>;
-    plan_visibility: {
-      visibility: string;
+        }
+      ];
+      veganAlternative: string | null;
     };
-    personal_information: {
-      name: string;
+    dinner: {
+      id: number;
+      mealName: string;
+      mealType: {
+        mealType: string;
+      };
+      instructions: string;
+      meal_ingredients: [
+        {
+          mealId: number;
+          ingredientName: string;
+          fat: number;
+          carbs: number;
+          protein: number;
+          calories: number;
+        }
+      ];
+      veganAlternative: string | null;
     };
-    exercise: [
-      {
-        exerciseName: string;
-        bodyPart: string;
-        equipment: string;
-        day: string;
-        exerciseDemo: string;
-        youtubeLink: string | null;
-        measurement: string;
-        instruction: string;
-        bmiClassification: number;
-        exerciseMeasurementType: number;
-        exercise_measurement_type: {
-          id: number;
-          measurement: string;
-        };
-        bmi_classification: {
-          classification: string;
-        };
-      }
-    ];
-  };
+  }>;
+}
+
+export interface exercisePlanInfo {
+  planName: string;
+  shortDescription?: string;
+  tags: Array<{
+    exercise_tags: {
+      id: number;
+      exerciseTagName: string;
+    };
+  }>
+}
+
+export interface mealPlanInfo {
+  planName: string;
+    shortDescription?: string;
+    tags: Array<{
+      meal_tags: {
+        id: number | string;
+        mealTagName: string;
+      };
+    }>
 }
