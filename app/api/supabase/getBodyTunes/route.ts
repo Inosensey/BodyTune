77963f23ed751c2f3259045ebb8b551d9a504347
Inosensey <dingcong.bae@gmail.com>
2025-04-01 22:05:03 +0800

    Small indentation adjustments.

--- app/api/supabase/getBodyTunes/route.ts
+++ app/api/supabase/getBodyTunes/route.ts
@@ -5,133 +5,133 @@ export async function GET() {
   const supabase = await createSSR();
 
   try {
-    const bodyTuneQuery = supabase.from("bodytune_plan").select(`
-                id,
-                mealPlanId,
-                exercisePlanId,
-                created_by,
-                personal_information:personal_information (
-                    name
-                ),
-                meal_plan (
-                    id,
-                    planName,
-                    created_by,
-                    visibility,
-                    created_by,
-                    personal_information (
-                        name
-                    ),
-                    meal_plan_tags (
-                        meal_tags (
-                            id,
-                            mealTagName
-                        )
-                    ),
-                    plan_visibility (
-                        visibility
-                    ),
-                    daily_meals (
-                        plan_id,
-                        day,
-                        breakFast,
-                        lunch,
-                        dinner,
-                        breakFast:meal!meal_plan_breakFast_fkey (
-                            id, 
-                            mealName,
-                            mealType,
-                            veganAlternative,
-                            instructions,
-                            mealType:meal_type (
-                                mealType
-                            ),
-                            meal_ingredients (
-                                mealId,
-                                ingredientName,
-                                calories,
-                                carbs,
-                                fat,
-                                protein
-                            )  
-                        ),
-                        lunch:meal!meal_plan_lunch_fkey (
-                            id, 
-                            mealName,
-                            mealType,
-                            veganAlternative,
-                            instructions,
-                            mealType:meal_type (
-                                mealType
-                            ),
-                            meal_ingredients (
-                                mealId,
-                                ingredientName,
-                                calories,
-                                carbs,
-                                fat,
-                                protein
-                            )  
-                        ),
-                        dinner:meal!meal_plan_dinner_fkey (
-                            id, 
-                            mealName,
-                            mealType,
-                            veganAlternative,
-                            instructions,
-                            mealType:meal_type (
-                                mealType
-                            ),
-                            meal_ingredients (
-                                mealId,
-                                ingredientName,
-                                calories,
-                                carbs,
-                                fat,
-                                protein
-                            )  
-                        )
-                    )
-                ),
-                exercise_plan (
-                    id,
-                    planName,
-                    created_by,
-                    visibility,
-                    plan_visibility (
-                        visibility
-                    ),
-                    exercise_plan_tag (
-                        exercise_tags (
-                            id,
-                            exerciseTagName
-                        )
-                    ),
-                    created_by,
-                    personal_information (
-                        name
-                    ),
-                    exercise (
-                        exerciseName,
-                        bodyPart,
-                        equipment,
-                        day,
-                        exerciseDemo,
-                        youtubeLink,
-                        measurement,
-                        instruction,
-                        bmiClassification,
-                        exerciseMeasurementType,
-                        exercise_measurement_type (
-                            id,
-                            measurement
-                        ),
-                        bmi_classification (
-                            classification
-                        )
-                    )
-                )`);
-    const {data, error} = await bodyTuneQuery;
+    const { data, error } = await supabase.from("bodytune_plan").select(`
+      id,
+      mealPlanId,
+      exercisePlanId,
+      created_by,
+      personal_information:personal_information (
+          name
+      ),
+      meal_plan (
+          id,
+          planName,
+          created_by,
+          visibility,
+          created_by,
+          personal_information (
+              name
+          ),
+          meal_plan_tags (
+              meal_tags (
+                  id,
+                  mealTagName
+              )
+          ),
+          plan_visibility (
+              visibility
+          ),
+          daily_meals (
+              plan_id,
+              day,
+              breakFast,
+              lunch,
+              dinner,
+              breakFast:meal!meal_plan_breakFast_fkey (
+                  id, 
+                  mealName,
+                  mealType,
+                  veganAlternative,
+                  instructions,
+                  mealType:meal_type (
+                      mealType
+                  ),
+                  meal_ingredients (
+                      mealId,
+                      ingredientName,
+                      calories,
+                      carbs,
+                      fat,
+                      protein
+                  )  
+              ),
+              lunch:meal!meal_plan_lunch_fkey (
+                  id, 
+                  mealName,
+                  mealType,
+                  veganAlternative,
+                  instructions,
+                  mealType:meal_type (
+                      mealType
+                  ),
+                  meal_ingredients (
+                      mealId,
+                      ingredientName,
+                      calories,
+                      carbs,
+                      fat,
+                      protein
+                  )  
+              ),
+              dinner:meal!meal_plan_dinner_fkey (
+                  id, 
+                  mealName,
+                  mealType,
+                  veganAlternative,
+                  instructions,
+                  mealType:meal_type (
+                      mealType
+                  ),
+                  meal_ingredients (
+                      mealId,
+                      ingredientName,
+                      calories,
+                      carbs,
+                      fat,
+                      protein
+                  )  
+              )
+          )
+      ),
+      exercise_plan (
+          id,
+          planName,
+          created_by,
+          visibility,
+          plan_visibility (
+              visibility
+          ),
+          exercise_plan_tag (
+              exercise_tags (
+                  id,
+                  exerciseTagName
+              )
+          ),
+          created_by,
+          personal_information (
+              name
+          ),
+          exercise (
+              exerciseName,
+              bodyPart,
+              equipment,
+              day,
+              exerciseDemo,
+              youtubeLink,
+              measurement,
+              instruction,
+              bmiClassification,
+              exerciseMeasurementType,
+              exercise_measurement_type (
+                  id,
+                  measurement
+              ),
+              bmi_classification (
+                  classification
+              )
+          )
+      )`
+    );
     if (error) {
       return Response.json({ message: error });
     }
