74e5b6ad5707f94d892ed24cad1d9a365f1751cf
Inosensey <dingcong.bae@gmail.com>
2025-04-03 22:20:44 +0800

    Added three new types: exercisePlanName, exercisePlanGeneralInfo, and exercisePlanTag.

--- types/exerciseTypes.ts
+++ types/exerciseTypes.ts
@@ -1,4 +1,23 @@
 export interface ExercisePlanInfoTypes {
-    exercisePlanName: string;
-    shortDescription?: string;
-  }
+  exercisePlanName: string;
+  shortDescription?: string;
+}
+
+export type exercisePlanName = {
+  selectedExercisePlan: number | string;
+  exercisePlanName: string;
+};
+
+export interface exercisePlanGeneralInfo {
+  id: number,
+  planName: string;
+  shortDescription?: string;
+  tags: Array<exercisePlanTag>;
+}
+
+export interface exercisePlanTag {
+  exercise_tags: {
+    id: number;
+    exerciseTagName: string;
+  };
+}
