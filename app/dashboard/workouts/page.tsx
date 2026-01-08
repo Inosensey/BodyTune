da89693debf35352ff2d093916a08272392bc315
Inosensey <dingcong.bae@gmail.com>
2026-01-08 10:42:37 +0800

    Updated the exercisePlans data source and its associated types.

--- app/dashboard/workouts/page.tsx
+++ app/dashboard/workouts/page.tsx
@@ -1,11 +1,20 @@
 "use server"
 
+// lib
+import { getUserExercisePlans } from "@/lib/supabaseQueries"
+
+// Components
 import BodyTuneWorkouts from "@/components/dashboardComponents/bodytuneWorkouts/BodyTuneWorkouts"
 
-const BodyTuneWorkoutsPage = () => {
+// Types
+import { exercisePlanQuery } from "@/types/planTypes"
+
+const BodyTuneWorkoutsPage = async () => {
+  const exercisePlans: Array<exercisePlanQuery> | [] = await getUserExercisePlans();
+
   return (
     <div className='px-4 mt-4 w-full'>
-      <BodyTuneWorkouts />
+      <BodyTuneWorkouts exercisePlans={exercisePlans} />
     </div>
   )
 }
