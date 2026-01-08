958c6cf804fb34f54eae9ca19461315306e724ad
Inosensey <dingcong.bae@gmail.com>
2026-01-08 10:42:04 +0800

    Made minor adjustments to the Supabase client.

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -5,7 +5,15 @@ import { createBrowserClient } from "@supabase/ssr";
 const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
 const supabaseAnonKey: string = process.env.NEXT_PUBLIC_ANON_KEY ?? "";
 
-export const useSupabase = createBrowserClient(
-  supabaseUrl,
-  supabaseAnonKey
-);
+// export const useSupabase = createBrowserClient(
+//   supabaseUrl,
+//   supabaseAnonKey
+// );
+
+export function createClient() {
+  // Create a supabase client on the browser with project's credentials
+  return createBrowserClient(
+    supabaseUrl,
+    supabaseAnonKey
+  );
+}
