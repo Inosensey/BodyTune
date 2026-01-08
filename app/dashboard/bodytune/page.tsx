b9a98a14c34b6d882684a4b592e3525ca3404678
Inosensey <dingcong.bae@gmail.com>
2026-01-08 10:42:36 +0800

    Updated the odyTunes data source and its associated types.

--- app/dashboard/bodytune/page.tsx
+++ app/dashboard/bodytune/page.tsx
@@ -1,7 +1,7 @@
 "use server"
 
 // lib
-import { getBodyTunes } from "@/lib/supabaseQueries"
+import { getUserBodyTunes } from "@/lib/supabaseQueries"
 
 // Components
 import BodyTuneStudio from "@/components/dashboardComponents/bodytuneStudio/BodyTuneStudio"
@@ -11,7 +11,7 @@ import { bodyTunePlan } from "@/types/planTypes";
 
 
 const BodyTuneStudioPage = async () => {
-  const  bodyTunes: Array<bodyTunePlan> | undefined = await getBodyTunes();
+  const  bodyTunes: Array<bodyTunePlan> | [] = await getUserBodyTunes();
 
   return (
     <div className='px-4 mt-4 w-full'>
