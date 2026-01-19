d1d3d252fbeed9113b4b0f6605bc0c538d102b8b
Inosensey <dingcong.bae@gmail.com>
2026-01-19 11:06:32 +0800

    Update profile page.

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -4,26 +4,52 @@ import Profile from "@/components/dashboardComponents/profile/Profile";
 
 // Libs
 import getUserInformation from "@/lib/getUserInformation";
+import {
+  getUserBodyTunes,
+  getUserExercisePlans,
+  getUserMealPlans,
+} from "@/lib/supabaseQueries";
 
 // Types
 import { TableRow } from "@/types/database.types";
+import {
+  bodyTunePlan,
+  exercisePlanQuery,
+  mealPlanQuery,
+} from "@/types/planTypes";
 
 const ProfilePage = async () => {
-  const res = await getUserInformation();
+  const [userRes, exercisePlanRes, mealPlanRes, bodyTuneRes]: [
+    Response | undefined,
+    Array<exercisePlanQuery>,
+    Array<mealPlanQuery>,
+    Array<bodyTunePlan>,
+  ] = await Promise.all([
+    getUserInformation(),
+    getUserExercisePlans(),
+    getUserMealPlans(),
+    getUserBodyTunes(),
+  ]);
+
   let userInformation:
     | { response: TableRow<"personal_information">[] }
     | { response: [] } = { response: [] };
-  if (res) {
-    userInformation = await res.json();
+  if (userRes) {
+    userInformation = await userRes.json();
   } else {
-    userInformation  = { response: [] };
+    userInformation = { response: [] };
   }
   const personalInformation:
     | { response: TableRow<"personal_information">[] }
     | [] = userInformation;
   return (
     <div className="w-full laptop:px-4 laptop:mt-4">
-      <Profile personalInfo={personalInformation.response[0]} />
+      <Profile
+        personalInfo={personalInformation.response[0]}
+        bodyTunes={bodyTuneRes}
+        exercisePlans={exercisePlanRes}
+        mealPlans={mealPlanRes}
+      />
     </div>
   );
 };
