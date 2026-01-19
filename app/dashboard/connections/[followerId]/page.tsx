c5cb7f82e6abd0bb34932fcd1fc9bdc44d9a386c
Inosensey <dingcong.bae@gmail.com>
2026-01-19 11:06:32 +0800

    Update follower page dynamic route component

--- app/dashboard/connections/[followerId]/page.tsx
+++ app/dashboard/connections/[followerId]/page.tsx
@@ -1,14 +1,47 @@
-"use server"
+"use server";
+
+// lib
+import {
+  getUserBodyTunes,
+  getUserExercisePlans,
+  getUserMealPlans,
+} from "@/lib/supabaseQueries";
 
 // Components
-import FollowerProfile from "@/components/dashboardComponents/connections/FollowerProfile"
+import FollowerProfile from "@/components/dashboardComponents/connections/FollowerProfile";
 
-const FollowerProfilePage = () => {
+// Types
+interface props {
+  params: { followerId: string };
+}
+import {
+  bodyTunePlan,
+  exercisePlanQuery,
+  mealPlanQuery,
+} from "@/types/planTypes";
+
+const FollowerProfilePage = async ({ params }: props) => {
+  const followerId: string = params.followerId;
+  const [exercisePlanRes, mealPlanRes, bodyTuneRes]: [
+    Array<exercisePlanQuery>,
+    Array<mealPlanQuery>,
+    Array<bodyTunePlan>,
+  ] = await Promise.all([
+    getUserExercisePlans(followerId),
+    getUserMealPlans(followerId),
+    getUserBodyTunes(followerId),
+  ]);
+
   return (
     <div className="w-full laptop:px-4 laptop:mt-4">
-        <FollowerProfile />
+      <FollowerProfile 
+        bodyTunes={bodyTuneRes}
+        exercisePlans={exercisePlanRes}
+        mealPlans={mealPlanRes}
+        followerId={followerId}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default FollowerProfilePage
+export default FollowerProfilePage;
